2fdc67715b113bf91092d1cd51422ed930edccae
Steffi <s.tenberg@unsw.edu.au>
2016-09-15 14:04:14 +1000

    prep presentation, setup.py for silQ

---
+++ setup.py
@@ -0,0 +1,98 @@
+from setuptools import setup, find_packages
+from distutils.version import StrictVersion
+from importlib import import_module
+import re
+
+def get_version(verbose=1):
+    """ Extract version information from source code """
+
+    try:
+        with open('silq/version.py', 'r') as f:
+            ln = f.readline()
+            # print(ln)
+            m = re.search('.* ''(.*)''', ln)
+            version = (m.group(1)).strip('\'')
+    except Exception as E:
+        print(E)
+        version = 'none'
+    if verbose:
+        print('get_version: %s' % version)
+    return version
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+extras = {
+    'Non-Linear Least-Squares Minimization and Curve-Fitting': ('lmfit', '0.9.5'),
+    'Peak finding': ('peakutils', '1.0.3')
+}
+extras_require = {k: '>='.join(v) for k, v in extras.items()}
+
+setup(name='silq',
+      version=get_version(),
+      use_2to3=False,
+      author='Serwan Asaad, Stefanie Tenberg',
+      author_email='serwan.asaad@gmail.com',
+      maintainer='Serwan Asaad, Stefanie Tenberg',
+      maintainer_email='serwan.asaad@gmail.com',
+      description='Python-based measurement framework for qubit measurements developed'
+                  ' by the research group of Andrea Morello in Sydney',
+      long_description=readme(),
+      url='https://github.com/nulinspiratie/SilQ',
+      classifiers=[
+          'Development Status :: 1 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Scientific/Engineering'
+      ],
+      license='Private',
+      packages=find_packages(),
+      package_data={'silq': ['widgets/*.js', 'widgets/*.css']},
+      install_requires=[
+          'numpy>=1.10',
+          'pyvisa>=1.8',
+          'IPython>=4.0',
+          'ipywidgets>=4.1',
+          'h5py>=2.6',
+      ],
+      extras_require=extras_require,
+      zip_safe=False)
+
+version_template = '''
+*****
+***** package {0} must be at least version {1}.
+***** Please upgrade it (pip install -U {0} or conda install {0})
+***** in order to use {2}
+*****
+'''
+
+missing_template = '''
+*****
+***** package {0} not found
+***** Please install it (pip install {0} or conda install {0})
+***** in order to use {1}
+*****
+'''
+
+valueerror_template = '''
+*****
+***** package {0} version not understood
+***** Please make sure the installed version ({1})
+***** is compatible with the minimum required version ({2})
+***** in order to use {3}
+*****
+'''
+
+# now test the versions of extras
+for extra, (module_name, min_version) in extras.items():
+    try:
+        module = import_module(module_name)
+        if StrictVersion(module.__version__) < StrictVersion(min_version):
+            print(version_template.format(module_name, min_version, extra))
+    except ImportError:
+        print(missing_template.format(module_name, extra))
+    except ValueError:
+        print(valueerror_template.format(
+            module_name, module.__version__, min_version, extra))
