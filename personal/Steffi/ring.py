de315f4b4230cb832c92394d70cb57058b26f664
Steffi <s.tenberg@unsw.edu.au>
2016-09-20 15:50:08 +1000

    finanlising presentation

--- personal/Steffi/ring.py
+++ personal/Steffi/ring.py
@@ -1,38 +1,36 @@
 class Ring():
-    def __init__(self, name, diameter, material, weight, owner, **kwargs):
+    def __init__(self, name, material, shape="round", **kwargs):
         self.name = name
-        self.diameter = diameter
         self.material = material
-        self.weight = weight
-        self.owner = owner
+        self.shape = shape
 
     def impress(self, value):
         shiny = value
         return shiny
 
-    def own(self):
-        self.owner = slave
-        return
+    def makes_you_feel(self):
+        return "rich and special"
 
 
 class SauronsRing(Ring):
-    def __init__(self, name, diameter, material, weight, inscription, owner,  **kwargs):
-        super().__init__(name, diameter, material, weight, **kwargs)
+    def __init__(self, name, material, inscription, **kwargs):
+        super().__init__(name, material, **kwargs)
 
         self.inscription = inscription
 
+    def makes_you_feel(self):
+        super().__init__()
+        return "special and allmighty"
 
-    def ruleThemAll(self, allhumans):
+    def rule_them_all(self, all_humans):
+        slaves = all_humans
         return slaves
 
 
 name = "Saurons Ring"
 material = "unknown"
-diameter = 1cm
-weight = 50g
 inscription = "A ring to rule them all"
-owner = "Sauron"
 AndreasGroup=['Andrea', 'Arne', 'Guilherme', ...]
 
-SauronsRing(name, diameter, material, weight, inscription, owner)
+SauronsRing(name, material, inscription)
 SauronsRing.ruleThemAll(AndreasGroup)
