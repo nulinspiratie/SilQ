8bbd871b0c96a7a959d3bb4058ca1f24c32a3b65
nulinspiratie <serwan.asaad@gmail.com>
2016-09-29 11:36:31 +1000

    Working on ArbStudio interface

--- silq/meta_instruments/instrument_interfaces/InstrumentInterface.py
+++ silq/meta_instruments/instrument_interfaces/InstrumentInterface.py
@@ -19,68 +19,14 @@ class InstrumentInterface():
         return np.any([pulse_implementation.is_implementation(pulse)
                        for pulse_implementation in self.pulse_implementations])
 
+    def setup(self):
+        pass
 
-class PulseImplementation():
-    def __init__(self, pulse_class, pulse_conditions=[]):
-        self.pulse_class = pulse_class
-        self.pulse_conditions = [PulseCondition(self, property, condition) for
-                                 (property, condition) in pulse_conditions]
+    def start(self):
+        pass
 
-    def add_pulse_condition(self, property, condition):
-        self.pulse_conditions == [PulseCondition(self, property, condition)]
-
-    def is_implementation(self, pulse):
-        if pulse.__class__ != self.pulse_class:
-            return False
-        else:
-            return np.all([pulse_condition.satisfies(pulse)
-                           for pulse_condition in self.pulse_conditions])
-
-
-class PulseCondition():
-    def __init__(self, pulse_class, property, condition):
-        self.property = property
-
-        self.verify_condition(condition)
-        self.condition = condition
-
-    def verify_condition(self, condition):
-        if type(condition) is list:
-            assert condition, "Condition must not be an empty list"
-        elif type(condition) is dict:
-            assert ('min' in condition or 'max' in condition), \
-                "Dictionary condition must have either a 'min' or a 'max'"
-
-    def satisfies(self, pulse):
-        """
-        Checks if a given pulse satisfies this Pulsecondition
-        Args:
-            pulse: Pulse to be checked
-
-        Returns: Bool depending on if the pulse satisfies PulseCondition
-
-        """
-        property_value = getattr(pulse, self.property)
-
-        # Test for condition
-        if type(self.condition) is dict:
-            # condition contains min and/or max
-            if 'min' in self.condition and \
-                            property_value < self.condition['min']:
-                return False
-            elif 'max' in self.condition and \
-                            property_value > self.condition['max']:
-                return False
-            else:
-                return True
-        elif type(self.condition) is list:
-            if property_value not in self.condition:
-                return False
-            else:
-                return True
-        else:
-            raise Exception("Cannot interpret pulse condition: {}".format(
-                self.condition))
+    def stop(self):
+        pass
 
 
 class Channel:
--- silq/meta_instruments/instrument_interfaces/lecroy/ArbStudio1104.py
+++ silq/meta_instruments/instrument_interfaces/lecroy/ArbStudio1104.py
@@ -1,5 +1,5 @@
 from silq.meta_instruments.instrument_interfaces \
-    import InstrumentInterface, Channel, PulseImplementation
+    import InstrumentInterface, Channel
 from silq.meta_instruments.PulseSequence import PulseSequence
 from silq.meta_instruments import pulses
 
@@ -14,24 +14,31 @@ class ArbStudio1104_Interface(InstrumentInterface):
         self.trigger_out_channel = Channel(name='trig_out', output_trigger=True)
 
         self.pulse_implementations = [
-            PulseImplementation(
-                pulses.SinePulse,
+            pulses.SinePulse.create_implementation(
                 pulse_conditions=('frequency', {'min':1e6, 'max':50e6})
             ),
-            PulseImplementation(
-                pulses.DCPulse,
+            pulses.DCPulse.create_implementation(
                 pulse_conditions=('amplitude', {'min': 0, 'max': 2.5})
             ),
-            PulseImplementation(
-                pulses.TriggerPulse,
+            pulses.TriggerPulse.create_implementation(
                 pulse_conditions=[]
             )
         ]
 
+    def setup(self):
+        # TODO implement setup for modes other than stepped
+        self.active_channels = set([pulse.connection.output_channel
+                        for pulse in self.pulse_sequence])
+        for ch in self.active_channels:
+            eval("self.instrument.ch{}_trigger_source('fp_trigger_in')".format(
+                ch))
+            eval("self.instrument.ch{}_trigger_mode('stepped')".format(ch))
+            eval('self.instrument.ch{}_clear_waveforms()'.format(ch))
 
-class ArbStudio1104_PulseSequence(PulseSequence):
-    def __init__(**kwargs):
-        super().__init__(**kwargs)
 
-    def add(self, pulse):
-        pass
+# class ArbStudio1104_PulseSequence(PulseSequence):
+#     def __init__(**kwargs):
+#         super().__init__(**kwargs)
+#
+#     def add(self, pulse):
+#         pass
