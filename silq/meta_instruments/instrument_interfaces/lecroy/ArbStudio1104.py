52e9817921a7a2965b61e0cf0948144c79bcefaa
nulinspiratie <serwan.asaad@gmail.com>
2016-09-29 10:49:06 +1000

    Finished pulse implementation

--- silq/meta_instruments/instrument_interfaces/lecroy/ArbStudio1104.py
+++ silq/meta_instruments/instrument_interfaces/lecroy/ArbStudio1104.py
@@ -1,5 +1,5 @@
 from silq.meta_instruments.instrument_interfaces \
-    import InstrumentInterface, Channel,
+    import InstrumentInterface, Channel, PulseImplementation
 from silq.meta_instruments.PulseSequence import PulseSequence
 from silq.meta_instruments import pulses
 
@@ -13,15 +13,20 @@ class ArbStudio1104_Interface(InstrumentInterface):
         self.trigger_in_channel = Channel(name='trig_in', input_trigger=True)
         self.trigger_out_channel = Channel(name='trig_out', output_trigger=True)
 
-        self.pulse_implementations = {
-            pulses.SinePulse: {'conditions': {
-                    'frequency': {'min': 1e6, 'max': 50e6}
-            }},
-            pulses.DCPulse: {'conditions': {
-                'amplitude': {'min': 0, 'max': 2.5}
-            }},
-            pulses.TriggerPulse: {'conditions': {}}
-        }
+        self.pulse_implementations = [
+            PulseImplementation(
+                pulses.SinePulse,
+                pulse_conditions=('frequency', {'min':1e6, 'max':50e6})
+            ),
+            PulseImplementation(
+                pulses.DCPulse,
+                pulse_conditions=('amplitude', {'min': 0, 'max': 2.5})
+            ),
+            PulseImplementation(
+                pulses.TriggerPulse,
+                pulse_conditions=[]
+            )
+        ]
 
 
 class ArbStudio1104_PulseSequence(PulseSequence):
