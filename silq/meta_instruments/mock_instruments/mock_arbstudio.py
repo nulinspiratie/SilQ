61ea27cea12da24e822b8b9708b073fde0b81d0e
nulinspiratie <serwan.asaad@gmail.com>
2016-09-30 15:07:04 +1000

    Improvements to Mock arbstudio

--- silq/meta_instruments/mock_instruments/mock_arbstudio.py
+++ silq/meta_instruments/mock_instruments/mock_arbstudio.py
@@ -1,6 +1,8 @@
 from functools import partial
 
 from qcodes import Instrument
+from qcodes.instrument.parameter import ManualParameter
+from qcodes.utils import validators as vals
 
 
 def print_function(*args, **kwargs):
@@ -9,13 +11,45 @@ def print_function(*args, **kwargs):
 class MockArbStudio(Instrument):
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
-        channels = ['ch1', 'ch2', 'ch3', 'ch4']
-        functions = ['trigger_source', 'trigger_mode',
-                     'add_waveform', 'sequence']
+        channels = [1, 2, 3, 4]
+        channel_functions = ['trigger_source', 'trigger_mode',
+                             'clear_waveforms']
+        # functions = ['load_waveforms', 'load_sequence']
+        # Initialize waveforms and sequences
+        self._waveforms = [[] for k in channels]
         for ch in channels:
-            for fn in functions:
-                pass
+            for fn in channel_functions:
                 self.add_parameter(
-                    '{}_{}'.format(ch, fn),
-                    set_cmd=partial(print_function, ch=ch, function=fn)
-                    )
+                    'ch{}_{}'.format(ch, fn),
+                    get_cmd=partial(print_function, ch=ch, function=fn,
+                                    mode='get'),
+                    set_cmd=partial(print_function, ch=ch, function=fn,
+                                    mode='set'),
+                    vals=vals.Anything())
+
+            self.add_function('ch{}_add_waveform'.format(ch),
+                              call_cmd=partial(self._add_waveform, ch),
+                              args=[vals.Anything()])
+
+            self.add_parameter('ch{}_sequence'.format(ch),
+                               parameter_class=ManualParameter,
+                               label='Channel {} Sequence'.format(ch),
+                               initial_value=[],
+                               vals=vals.Anything())
+
+
+    def _add_waveform(self, channel, waveform):
+        print_function(channel=channel, function='add_waveform',
+                       waveform=waveform)
+        assert len(waveform)%2 == 0, 'Waveform must have an even number of points'
+        assert len(waveform)> 2, 'Waveform must have at least four points'
+        self._waveforms[channel - 1].append(waveform)
+
+    def load_waveforms(self, channels=[]):
+        print_function(channels=channels, function='load_waveforms')
+
+    def load_sequence(self, channels=[]):
+        print_function(channels=channels, function='load_sequence')
+
+    def get_waveforms(self):
+        return self._waveforms
