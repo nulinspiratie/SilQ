7a16aa1bdbbe475408c9446b448ca8e569970cd3
nulinspiratie <serwan.asaad@gmail.com>
2016-09-30 17:08:29 +1000

    Implementing pulesblaster into setup

--- silq/instrument_interfaces/__init__.py
+++ silq/instrument_interfaces/__init__.py
@@ -6,10 +6,14 @@ from .interface import InstrumentInterface, Channel
 def get_instrument_interface(instrument):
     from .lecroy.ArbStudio1104_interface import ArbStudio1104Interface
     from .chip_interface import ChipInterface
+    from .spincore.PulseBlasterESRPRO_interface import \
+        PulseBlasterESRPROInterface
 
     instrument_interfaces = {
         'ArbStudio1104': ArbStudio1104Interface,
         'MockArbStudio': ArbStudio1104Interface,
+        'PulseBlasterESRPRO': PulseBlasterESRPROInterface,
+        'MockPulseBlaster': PulseBlasterESRPROInterface,
         'Chip': ChipInterface
     }
 
@@ -18,10 +22,6 @@ def get_instrument_interface(instrument):
     if instrument_name == 'RemoteInstrument':
         instrument_name = instrument._instrument_class.__name__
 
-    # if instrument_name == 'ArbStudio1104':
-    #     from .lecroy.ArbStudio1104_interface import ArbStudio1104Interface
-    #     instrument_interface_class = ArbStudio1104Interface
-    # elif
     instrument_interface_class = instrument_interfaces[instrument_name]
 
     instrument_interface = instrument_interface_class(instrument)
