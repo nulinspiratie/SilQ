18438fd17fe79fe487460df9a9ae0c3a81a5c73f
nulinspiratie <serwan.asaad@gmail.com>
2016-10-03 12:52:05 +1100

    interfaces are now instruments living on same server

--- silq/instrument_interfaces/__init__.py
+++ silq/instrument_interfaces/__init__.py
@@ -18,11 +18,12 @@ def get_instrument_interface(instrument):
     }
 
     # TODO Need to find correct name of instrument
-    instrument_name = instrument.__class__.__name__
-    if instrument_name == 'RemoteInstrument':
-        instrument_name = instrument._instrument_class.__name__
+    instrument_class = instrument.__class__.__name__
+    if instrument_class == 'RemoteInstrument':
+        instrument_class = instrument._instrument_class.__name__
 
-    instrument_interface_class = instrument_interfaces[instrument_name]
+    instrument_interface_class = instrument_interfaces[instrument_class]
 
-    instrument_interface = instrument_interface_class(instrument)
+    instrument_interface = instrument_interface_class(instrument_name=instrument.name,
+                                                      server_name=instrument._server_name)
     return instrument_interface
