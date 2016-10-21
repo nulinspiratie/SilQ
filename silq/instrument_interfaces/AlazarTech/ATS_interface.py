8342479becc92d8d71366fc349076de316f6cd9c
nulinspiratie <serwan.asaad@gmail.com>
2016-10-21 17:08:46 +1100

    Work on ATS interface

--- silq/instrument_interfaces/AlazarTech/ATS_interface.py
+++ silq/instrument_interfaces/AlazarTech/ATS_interface.py
@@ -8,13 +8,22 @@ from qcodes.utils import validators as vals
 from qcodes.instrument_drivers.AlazarTech.ATS import AcquisitionController
 
 
-class ATSInterface(InstrumentInterface, AcquisitionController):
-
-    def __init__(self, name, instrument_name, **kwargs):
+class ATSInterface(InstrumentInterface):
+    def __init__(self, name, instrument_name, acquisition_controller_names,
+                 **kwargs):
         InstrumentInterface.__init__(name, instrument_name, **kwargs)
 
+        # Create a dictionary of the acquisition controller classes along with
+        # the acquisition controllers
+        self.acquisition_controllers = {}
+        for acquisition_controller_name in acquisition_controller_names:
+            acquisition_controller = self.find_instrument(
+                acquisition_controller_name)
+            cls = acquisition_controller._instrument_class.__name__
+            self.acquisition_controllers[cls] = acquisition_controller
+
         self.acquisition_settings = {}
-        # Obtain a list of all valid ATS acquisition kwargs
+        # Obtain a list of all valid ATS acquisition settings
         self._acquisition_settings_names = \
             inspect.signature(self.instrument.acquire).parameters.keys()
 
@@ -26,6 +35,15 @@ class ATSInterface(InstrumentInterface, AcquisitionController):
                            get_cmd=self._acquisition,
                            shapes=((),),
                            snapshot_value=False)
+
+        self.add_parameter(name='active_acquisition_controller',
+                           vals=vals.Enum('auto',
+                                          *self.acquisition_controllers.keys()))
+
+    def setup(self):
+        # TODO change when necessary
+        # acquisition_controller
+        pass
 
     def _acquisition(self):
         raise NotImplementedError(
@@ -43,9 +61,9 @@ class ATSInterface(InstrumentInterface, AcquisitionController):
             setting: acquisition kwarg to look for
 
         Returns:
-            Value of the acquisition kwarg
+            Value of the acquisition setting
         """
-        assert setting in self._settings_names, \
+        assert setting in self._acquisition_settings_names, \
             "Kwarg {} is not a valid ATS acquisition setting".format(setting)
         if setting in self.settings.keys():
             return self.settings[setting]
@@ -54,4 +72,9 @@ class ATSInterface(InstrumentInterface, AcquisitionController):
             return self.instrument.parameters[setting].get_latest()
 
     def update_acquisition_settings(self, **settings):
+        settings_valid = all(map(
+            lambda setting: setting in self._acquisition_settings_names,
+            settings.keys()))
+        assert settings_valid, \
+            'Not all settings are valid ATS acquisition settings'
         self.acquisition_settings.update(**settings)
