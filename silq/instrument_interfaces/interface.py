7aabc247095dd7a655c133e21c7f1b0854febd5b
nulinspiratie <serwan.asaad@gmail.com>
2016-10-26 16:41:19 +1100

    Added interface.get_final_required_pulses

--- silq/instrument_interfaces/interface.py
+++ silq/instrument_interfaces/interface.py
@@ -90,6 +90,10 @@ class InstrumentInterface(Instrument):
             raise Exception(
                 'Pulse sequence command {} not understood'.format(val))
 
+    def get_final_required_pulses(self):
+        raise NotImplementedError(
+            'This method should be implemented in a subclass')
+
     def setup(self):
         raise NotImplementedError(
             'This method should be implemented in a subclass')
