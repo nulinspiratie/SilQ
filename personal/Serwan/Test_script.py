51b07fed8ea6eb538f45be09d8fab246ae17581e
nulinspiratie <serwan.asaad@gmail.com>
2016-10-27 19:15:17 +1100

    Found bug in setting up acquisition channels

--- personal/Serwan/Test_script.py
+++ personal/Serwan/Test_script.py
@@ -1,58 +1,67 @@
+# Imports
 from functools import partial
 from importlib import reload
 import unittest
 
-from silq.pulses import PulseSequence, DCPulse, TriggerPulse, SinePulse
-from silq.instrument_interfaces import get_instrument_interface
-from silq.meta_instruments.chip import Chip
-from silq.meta_instruments.layout import Layout
-from silq.meta_instruments.mock_instruments import MockArbStudio
-
+import qcodes as qc
 from qcodes import Instrument
-from functools import partial
-from importlib import reload
-import unittest
+qc.config['core']['legacy_mp'] = True
+qc.loops.USE_MP = True
 
-from silq.pulses import PulseSequence, DCPulse, TriggerPulse, SinePulse
-from silq.instrument_interfaces import get_instrument_interface
+import silq
+from silq.pulses import PulseSequence, DCPulse, TriggerPulse, SinePulse, MeasurementPulse
+from silq.instrument_interfaces import get_instrument_interface, Channel
 from silq.meta_instruments.chip import Chip
-from silq.meta_instruments.layout import Layout
-from silq.meta_instruments.mock_instruments import MockArbStudio, MockPulseBlaster
+from silq.meta_instruments.layout import Layout, Connection, SingleConnection
+from silq.meta_instruments.mock_instruments import MockArbStudio, MockPulseBlaster, MockATS, MockAcquisitionController
 
 
 
 if __name__ == "__main__":
-
     class TestLayout(unittest.TestCase):
         def setUp(self):
-            self.arbstudio = MockArbStudio(name='arbstudio')
-            self.pulseblaster = MockPulseBlaster(name='pulseblaster')
-            self.chip = Chip(name='chip')
+            silent = True
+            self.arbstudio = MockArbStudio(name='arbstudio', silent=silent,
+                                           server_name='')
+            self.pulseblaster = MockPulseBlaster(name='pulseblaster',
+                                                 silent=silent, server_name='')
+            self.chip = Chip(name='chip', server_name='')
 
-            self.instruments = [self.arbstudio, self.pulseblaster, self.chip]
-            self.instrument_interfaces = {
+            self.ATS = MockATS(name='ATS', server_name='ATS_server',
+                               silent=silent)
+            self.ATS.config(sample_rate=100000)
+            self.acquisition_controller = MockAcquisitionController(
+                name='basic_acquisition_controller',
+                alazar_name='ATS',
+                server_name='ATS_server',
+                silent=silent)
+
+            self.instruments = [self.arbstudio, self.pulseblaster, self.chip,
+                                self.ATS]
+            self.interfaces = {
             instrument.name: get_instrument_interface(instrument)
             for instrument in self.instruments}
-            self.layout = Layout(name='Layout',
+
+            self.interfaces['ATS'].add_acquisition_controller(
+                'basic_acquisition_controller',
+                cls_name='Basic_AcquisitionController')
+            self.layout = Layout(name='layout',
                                  instrument_interfaces=list(
-                                     self.instrument_interfaces.values()),
-                                 server_name='layout')
-            self.instruments += [self.layout]
+                                     self.interfaces.values()),
+                                 server_name='layout_server')
 
-            self.arbstudio.silent(True)
-            self.pulseblaster.silent(True)
-
-            # print('Layout instruments: {}'.format(self.layout.instruments()))
-
-            # instruments that need to be closed during tear down
+            self.instruments += [self.acquisition_controller, self.layout]
 
         def tearDown(self):
             for instrument in self.instruments:
                 instrument.close()
-            for instrument_interface in self.instrument_interfaces.values():
+            for instrument_interface in self.interfaces.values():
                 instrument_interface.close()
+
         def test_arbstudio_pulseblaster(self):
+            self.layout.acquisition_outputs([])
             self.layout.primary_instrument('pulseblaster')
+            self.layout.acquisition_instrument('ATS')
             self.layout.add_connection(output_arg='arbstudio.ch1',
                                        input_arg='chip.TGAC')
             self.layout.add_connection(output_arg='arbstudio.ch2',
@@ -64,21 +73,41 @@ if __name__ == "__main__":
 
             trigger_connections = self.layout.get_connections(
                 input_instrument='arbstudio', trigger=True)
+            self.assertEqual(len(trigger_connections), 1)
             trigger_connection = trigger_connections[0]
+            self.assertEqual(trigger_connection.output['instrument'],
+                             'pulseblaster')
 
             pulse_sequence = PulseSequence()
             empty_pulse = DCPulse(name='empty', t_start=0, duration=10,
                                   amplitude=1.5)
-            #         load_pulse = DCPulse(name='load', t_start=10, duration=10, amplitude=-1.5)
-            #         read_pulse = DCPulse(name='read', t_start=20, duration=10, amplitude=0)
-            #         pulses = [empty_pulse, load_pulse, read_pulse]
-            pulses = [empty_pulse]
+            load_pulse = DCPulse(name='load', t_start=10, duration=10,
+                                 amplitude=-1.5)
+            read_pulse = DCPulse(name='read', t_start=20, duration=10,
+                                 amplitude=0)
+            pulses = [empty_pulse, load_pulse, read_pulse]
             for pulse in pulses:
                 pulse_sequence.add(pulse)
 
             self.layout.target_pulse_sequence(pulse_sequence)
-            print(self.instrument_interfaces['arbstudio'].pulse_sequence())
-            print(self.instrument_interfaces['pulseblaster'].pulse_sequence())
+            self.layout.setup()
+
+            self.assertEqual(len(self.pulseblaster.instructions()), 7)
+            self.assertEqual(
+                [ins[0] for ins in self.pulseblaster.instructions()],
+                [1, 0, 1, 0, 1, 0, 0])
+            self.assertEqual(self.pulseblaster.instructions()[-1][2], 1)
+
+            self.pulseblaster.instructions([])
+            self.interfaces['pulseblaster'].ignore_first_trigger(True)
+            self.layout.target_pulse_sequence(pulse_sequence)
+            self.layout.setup()
+
+            self.assertEqual(len(self.pulseblaster.instructions()), 7)
+            self.assertEqual(
+                [ins[0] for ins in self.pulseblaster.instructions()],
+                [0, 0, 1, 0, 1, 0, 1])
+
 
     test = TestLayout()
     test.setUp()
