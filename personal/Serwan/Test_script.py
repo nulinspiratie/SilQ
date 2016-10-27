38eecd448d0ec068bc48ad0f58d0ffe8a1cdd7e8
nulinspiratie <serwan.asaad@gmail.com>
2016-10-27 19:51:28 +1100

    All tests working. Only remaining is combinedconnection+input_pulse_sequence

--- personal/Serwan/Test_script.py
+++ personal/Serwan/Test_script.py
@@ -58,33 +58,37 @@ if __name__ == "__main__":
             for instrument_interface in self.interfaces.values():
                 instrument_interface.close()
 
-        def test_arbstudio_pulseblaster(self):
-            self.layout.acquisition_outputs([])
+        def test_full_setup(self):
+            # Setup connectivity to match the Berdina setup
             self.layout.primary_instrument('pulseblaster')
             self.layout.acquisition_instrument('ATS')
+            self.layout.add_connection(output_arg='pulseblaster.ch1',
+                                       input_arg='arbstudio.trig_in',
+                                       trigger=True)
+            self.layout.add_connection(output_arg='pulseblaster.ch2',
+                                       input_arg='ATS.trig_in',
+                                       trigger=True)
+
             self.layout.add_connection(output_arg='arbstudio.ch1',
                                        input_arg='chip.TGAC')
             self.layout.add_connection(output_arg='arbstudio.ch2',
                                        input_arg='chip.DF', default=True)
+            self.layout.add_connection(output_arg='arbstudio.ch3',
+                                       input_arg='ATS.chC')
 
-            self.layout.add_connection(output_arg='pulseblaster.ch1',
-                                       input_arg='arbstudio.trig_in',
-                                       trigger=True)
+            self.layout.add_connection(output_arg='chip.output',
+                                       input_arg='ATS.chA')
+            self.layout.acquisition_outputs([('chip.output', 'output'),
+                                             ('arbstudio.ch3', 'pulses')])
 
-            trigger_connections = self.layout.get_connections(
-                input_instrument='arbstudio', trigger=True)
-            self.assertEqual(len(trigger_connections), 1)
-            trigger_connection = trigger_connections[0]
-            self.assertEqual(trigger_connection.output['instrument'],
-                             'pulseblaster')
-
+            # Setup pulses
             pulse_sequence = PulseSequence()
             empty_pulse = DCPulse(name='empty', t_start=0, duration=10,
                                   amplitude=1.5)
             load_pulse = DCPulse(name='load', t_start=10, duration=10,
                                  amplitude=-1.5)
             read_pulse = DCPulse(name='read', t_start=20, duration=10,
-                                 amplitude=0)
+                                 amplitude=0, acquire=True)
             pulses = [empty_pulse, load_pulse, read_pulse]
             for pulse in pulses:
                 pulse_sequence.add(pulse)
@@ -92,24 +96,38 @@ if __name__ == "__main__":
             self.layout.target_pulse_sequence(pulse_sequence)
             self.layout.setup()
 
+            # Test Pulseblaster
             self.assertEqual(len(self.pulseblaster.instructions()), 7)
             self.assertEqual(
                 [ins[0] for ins in self.pulseblaster.instructions()],
-                [1, 0, 1, 0, 1, 0, 0])
+                [1, 0, 1, 0, 3, 0, 0])
             self.assertEqual(self.pulseblaster.instructions()[-1][2], 1)
 
-            self.pulseblaster.instructions([])
-            self.interfaces['pulseblaster'].ignore_first_trigger(True)
-            self.layout.target_pulse_sequence(pulse_sequence)
-            self.layout.setup()
-
-            self.assertEqual(len(self.pulseblaster.instructions()), 7)
+            # Test ATS
+            ATS_pulse_sequence = self.interfaces['ATS'].pulse_sequence()
             self.assertEqual(
-                [ins[0] for ins in self.pulseblaster.instructions()],
-                [0, 0, 1, 0, 1, 0, 1])
+                self.interfaces['ATS'].active_acquisition_controller(),
+                'basic_acquisition_controller')
+            self.assertEqual(self.interfaces['ATS'].trigger_slope(), 'positive')
+            self.assertTrue(
+                0 < self.interfaces['ATS'].trigger_threshold() < 3.3)
+            self.assertEqual(len(ATS_pulse_sequence), 2)
+            configuration_settings = self.ATS.configuration_settings()
+            acquisition_settings = self.acquisition_controller.acquisition_settings()
+            self.assertEqual(configuration_settings['trigger_engine1'], 'J')
+            self.assertEqual(acquisition_settings['channel_selection'], 'AC')
+            self.assertEqual(configuration_settings['sample_rate'], 100000)
+
+            # Test acquisition
+            self.layout.start()
+            signal = self.layout.acquisition()
+            self.assertEqual(len(signal), 2)
+            for ch_label in ['output', 'pulses']:
+                print(self.layout.acquisition)
+                print(self.layout.acquisition.names)
 
 
     test = TestLayout()
     test.setUp()
-    test.test_arbstudio_pulseblaster()
+    test.test_full_setup()
     test.tearDown()
