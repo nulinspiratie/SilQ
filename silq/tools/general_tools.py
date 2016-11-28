d53790e09f53ab299ea1f70295fb5050b8281f0e
NML <serwan.asaad@gmail.com>
2016-11-28 18:51:57 +1100

    Added memory function

--- silq/tools/general_tools.py
+++ silq/tools/general_tools.py
@@ -29,3 +29,12 @@ def get_truth(test_val, target_val, relation='=='):
            '<=': operator.le,
            '==': operator.eq}
     return ops[relation](test_val, target_val)
+
+
+def get_memory_usage():
+    # return the memory usage in MB
+    import os
+    import psutil
+    process = psutil.Process(os.getpid())
+    mem = process.memory_info()[0] / float(2 ** 20)
+    return mem
