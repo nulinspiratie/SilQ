ef896118e78f45054a98c72de0976d2e02fbfd84
NML <serwan.asaad@gmail.com>
2016-12-05 16:15:13 +1100

    Fixed allocated_buffers error

--- silq/instrument_interfaces/AlazarTech/ATS_interface.py
+++ silq/instrument_interfaces/AlazarTech/ATS_interface.py
@@ -300,8 +300,7 @@ class ATSInterface(InstrumentInterface):
             if buffers_per_acquisition > 1:
                 allocated_buffers = 2
             else:
-                # TODO figure out why 1 buffer does not work
-                allocated_buffers = 2
+                allocated_buffers = 1
 
             self.update_settings(samples_per_record=samples_per_record,
                                  records_per_buffer=records_per_buffer,
