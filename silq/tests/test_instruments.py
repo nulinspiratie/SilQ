75bca19ca3856197f8354d6d2c08c64286e4ebae
NML <serwan.asaad@gmail.com>
2016-12-14 08:33:27 +1100

    Fixed Pulse config bug

--- silq/tests/test_instruments.py
+++ silq/tests/test_instruments.py
@@ -1,5 +1,6 @@
 from qcodes import Instrument
 from qcodes.utils import validators as vals
+from qcodes.instrument.parameter import ManualParameter
 
 class TestInnerInstrument(Instrument):
     def __init__(self, name, **kwargs):
@@ -22,6 +23,9 @@ class TestInstrument(Instrument):
         super().__init__(name=name, **kwargs)
         self.add_parameter(name='x_val',
                            get_cmd=lambda: self.x,
+                           vals=vals.Anything())
+        self.add_parameter(name='pulse',
+                           parameter_class=ManualParameter,
                            vals=vals.Anything())
 
     def set_x(self, val):
