36b2e79fdb40e33d76be65eab9ff069bf8e0ef7a
NML <serwan.asaad@gmail.com>
2016-12-13 13:49:18 +1100

    Work on 1T msmts + cleaning up t1 notebook

--- experiments/EWJN/init/0_imports.py
+++ experiments/EWJN/init/0_imports.py
@@ -21,6 +21,7 @@ from qcodes.instrument.parameter import Parameter, ManualParameter, StandardPara
 from silq.parameters import measurement_parameters, general_parameters
 from silq.instrument_interfaces import get_instrument_interface
 from silq.pulses import *
+from silq.functions.general_functions import printattr
 
 if not 'USE_MP' in globals():
     USE_MP = True
