7676ccdb18723ed2e33bab76bb51e2c85b91c001
NML <serwan.asaad@gmail.com>
2016-12-14 14:49:48 +1100

    Added properties_config to parameters

--- experiments/EWJN/init/6_parameters.py
+++ experiments/EWJN/init/6_parameters.py
@@ -10,16 +10,17 @@ adiabatic_NMR_parameter = measurement_parameters.AdiabaticSweep_Parameter(
     layout=layout, mode='ESR')
 find_ESR_parameter = measurement_parameters.FindESR_Parameter(layout=layout,
                                                               mode='ESR')
+dark_counts_parameter = measurement_parameters.dark_counts_parameter(layout=layout)
 
 turnon_parameter = general_parameters.CombinedParameter(parameters=[TG, LB, RB])
 TGAC_DF_DS = general_parameters.CombinedParameter(parameters=[TGAC, DF, DS])
 LB_RB = general_parameters.CombinedParameter(parameters=[LB, RB])
 
 # Add all our instruments and parameters for logging
-station = qc.Station(SIM900, arbstudio, pulseblaster,
-                     ATS, triggered_controller, continuous_controller,
-                     keysight, layout,
-                     DC_parameter, EPR_parameter, T1_parameter,
-                     variable_read_parameter, adiabatic_ESR_parameter,
-                     adiabatic_NMR_parameter, find_ESR_parameter,
-                     *SIM900_scaled_parameters)
+station = qc.Station(
+    SIM900, arbstudio, pulseblaster, ATS, triggered_controller,
+    continuous_controller, keysight, layout,
+    DC_parameter, EPR_parameter, T1_parameter, variable_read_parameter,
+    adiabatic_ESR_parameter, adiabatic_NMR_parameter, find_ESR_parameter,
+    dark_counts_parameter,
+    *SIM900_scaled_parameters)
