21ff419efe8702775e7ed83e569b6afcf7eb2795
NML <serwan.asaad@gmail.com>
2016-12-14 14:12:18 +1100

    Major streamlining of pulses

--- silq/tests/test_parameters.py
+++ silq/tests/test_parameters.py
@@ -20,3 +20,14 @@ class TestValParameter(Parameter):
 
     def set(self, val):
         self.val = val
+
+class TestLoopParameter(Parameter):
+    def __init__(self, name, param, **kwargs):
+        self.val = 0
+        self.param = param
+        super().__init__(name, **kwargs)
+
+    def get(self):
+        qc.Loop(self.param[0:5:1], delay=0.5).each(self.param).run(
+            name='inner_loop', background=False, data_manager=False)
+        return self.param()
