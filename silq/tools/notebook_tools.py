2a56a8d7d1afff296bce083ea0e5c82048d26feb
NML <serwan.asaad@gmail.com>
2016-12-30 12:15:59 +1100

    Improved cell creation

--- silq/tools/notebook_tools.py
+++ silq/tools/notebook_tools.py
@@ -1,38 +1,38 @@
 from IPython.display import display_javascript
 
 
-def cell_create_below_execute(text):
-    """
-    Creates a cell below the current one in a notebook, adds text and executes
-    Args:
-        text: Code to be run in the cell
-
-    Returns:
-        None
-    """
+def create_cell(text, location='below', execute=False, select=True):
     text = text.replace('\n', '\\n')
-    display_javascript("""
-    var current_cell = Jupyter.notebook.get_selected_cell();
-    var current_index = Jupyter.notebook.find_cell_index(current_cell);
-    var new_cell = Jupyter.notebook.insert_cell_below('code', current_index);
-    new_cell.set_text('{}');
-    current_cell.unselect();
-    new_cell.execute();""".format(text), raw=True)
+    text = text.replace("'", '"')
 
-def cell_create_below_select(text):
-    """
-    Creates a cell below the current one in a notebook, adds text and executes
-    Args:
-        text: Code to be run in the cell
+    code = ''
+    # Create cell at specified location
+    if location == 'below':
+        code += """
+        var current_cell = Jupyter.notebook.get_selected_cell();
+        var current_index = Jupyter.notebook.find_cell_index(current_cell);
+        var new_cell = Jupyter.notebook.insert_cell_below('code', current_index);
+        """
+    elif location == 'bottom':
+        code += """
+        var current_cell = Jupyter.notebook.get_selected_cell();
+        var new_cell = Jupyter.notebook.insert_cell_at_bottom('code');
+        """
+    else:
+        raise Exception('Location {} not understood'.format(location))
 
-    Returns:
-        None
-    """
-    text = text.replace('\n', '\\n')
-    display_javascript("""
-    var current_cell = Jupyter.notebook.get_selected_cell();
-    var current_index = Jupyter.notebook.find_cell_index(current_cell);
-    var new_cell = Jupyter.notebook.insert_cell_below('code', current_index);
-    new_cell.set_text('{}');
-    current_cell.unselect();
-    new_cell.select();""".format(text), raw=True)
+    # Add text to cell
+    code += "new_cell.set_text('{}');".format(text);
+
+    # Select cell
+    if select:
+        code += """
+        current_cell.unselect();
+        new_cell.select();"""
+
+    # Execute cell
+    if execute:
+        code += "new_cell.execute();"
+
+    # Run code
+    display_javascript(code, raw=True)
