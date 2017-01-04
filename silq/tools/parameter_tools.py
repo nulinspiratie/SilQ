68f6a5c9b0f755f9994e34985233f2d0cbb3eccd
NML <serwan.asaad@gmail.com>
2017-01-04 15:39:16 +1100

    Working on Measurement (+other small)

--- silq/tools/parameter_tools.py
+++ silq/tools/parameter_tools.py
@@ -1,10 +1,10 @@
 import qcodes as qc
 import numpy as np
-
 parameter_config = qc.config['user']['properties'].get('parameters', {})
 
-def create_set_vals(num_parameters=None, steps=None, step_percentages=None,
-                    points=9, window=None, set_parameters=None, silent=True):
+def create_set_vals(num_parameters=None, step=None, step_percentage=None,
+                    points=None, window=None, set_parameters=None, silent=True,
+                    center_val=None, min_val=None, max_val=None, reverse=False):
     def calculate_step(min_step, max_step, percentage, round_vals=True):
         val = min_step * (max_step / min_step) ** (percentage / 100)
         if round_vals:
@@ -14,40 +14,37 @@ def create_set_vals(num_parameters=None, steps=None, step_percentages=None,
 
         return val
 
-    def determine_step(set_parameter, k=None):
-        if steps is not None:
-            if hasattr(steps, "__iter__"):
-                step = steps[k]
-            else:
-                step = steps
-        if step_percentages is not None:
-            if hasattr(steps, "__iter__"):
-                step_percentage = step_percentages[k]
-            else:
-                step_percentage = step_percentages
-            min_step, max_step = \
-            parameter_config[set_parameter.name]['steps']
-            step = calculate_step(min_step, max_step, step_percentage)
-        return step
-
-    def create_vals(set_parameter, k=None):
-        if hasattr(points, "__iter__"):
-            pts = points[k]
+    def determine_step(set_parameter):
+        if step_percentage is not None:
+            min_step, max_step = parameter_config[set_parameter.name]['steps']
+            return calculate_step(min_step, max_step, step_percentage)
         else:
-            pts = points
+            return step
 
-        center_val = set_parameter()
-        if steps is not None or step_percentages is not None:
-            pass
-            step = determine_step(set_parameter, k)
-            min_val = center_val - step * (pts - 1) / 2
-            max_val = center_val + step * (pts - 1) / 2
-        else:
+    def create_vals(set_parameter, points=None, window=None,
+                    center_val=None, min_val=None, max_val=None):
+        if window is None:
+            if min_val is not None and max_val is not None:
+                window = max_val - min_val
+            else:
+                step = determine_step(set_parameter)
+                window = step * (points - 1)
+        if points is None:
+            step = determine_step(set_parameter)
+            points = int(round(window / step))
+
+        if min_val is None and max_val is None:
+            if center_val is None:
+                center_val = set_parameter()
             min_val = center_val - window / 2
             max_val = center_val + window / 2
-        vals = list(np.linspace(min_val, max_val, pts))
+
+        vals = list(np.linspace(min_val, max_val, points))
+        if reverse:
+            vals.reverse()
 
         if not silent:
+            step = window / points
             print('{param}[{min_val}:{max_val}:{step}]'.format(
                 param=set_parameter, min_val=min_val, max_val=max_val,
                 step=step))
@@ -57,20 +54,20 @@ def create_set_vals(num_parameters=None, steps=None, step_percentages=None,
     if set_parameters is None:
         # Get default parameters from station
         station = qc.station.Station.default
-        set_parameters_names = parameter_config[str(num_parameters)]
+        set_parameter_names = parameter_config[str(num_parameters)]
         set_parameters = [getattr(station, name) for name in
-                          set_parameters_names]
-    else:
-        num_parameters = len(set_parameters)
+                          set_parameter_names]
 
     set_vals = []
-    if num_parameters == 0:
-        pass
-    elif num_parameters == 1:
-        vals = create_vals(set_parameters[0], 0)
-        set_vals = [set_parameters[0][vals]]
-    elif num_parameters == 2:
+    if isinstance(set_parameters, list):
         for k, set_parameter in enumerate(set_parameters):
-            vals = create_vals(set_parameter, k)
+            vals = create_vals(set_parameter, points=points, window=window,
+                               center_val=center_val, min_val=min_val,
+                               max_val=max_val)
             set_vals.append(set_parameter[vals])
+    else:
+        vals = create_vals(set_parameters, points=points, window=window,
+                           center_val=center_val, min_val=min_val,
+                           max_val=max_val)
+        set_vals = set_parameters[vals]
     return set_vals
