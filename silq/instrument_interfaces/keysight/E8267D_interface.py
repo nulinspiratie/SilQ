94b2e1578fa38fb90acb9c9d763750282a90fada
NML <serwan.asaad@gmail.com>
2017-01-19 17:39:30 +1100

    Added sine pulse to E8267D

--- silq/instrument_interfaces/keysight/E8267D_interface.py
+++ silq/instrument_interfaces/keysight/E8267D_interface.py
@@ -65,26 +65,33 @@ class E8267DInterface(InstrumentInterface):
 
         frequencies = [pulse.implement()['frequency']
                        for pulse in self._pulse_sequence]
-        deviations = [pulse.implement()['deviation']
-                      for pulse in self._pulse_sequence]
+
         powers = [pulse.power for pulse in self._pulse_sequence]
         assert len(set(frequencies)) == 1, "Cannot handle multiple frequencies"
-        assert len(set(deviations)) == 1, "Cannot handle multiple deviations"
         assert len(set(powers)) == 1, "Cannot handle multiple pulse powers"
 
+        if any('deviation' in pulse.implement()
+               for pulse in self._pulse_sequence):
+            deviations = [pulse.implement()['deviation']
+                    for pulse in self._pulse_sequence]
+            assert len(set(deviations)) == 1, "Cannot handle multiple " \
+                                              "deviations"
+            deviation = deviations[0]
+            self.instrument.frequency_deviation(deviation)
+            self.instrument.frequency_modulation('on')
+            self.instrument.frequency_modulation_source(
+                self.modulation_channel())
+        else:
+            self.instrument.frequency_deviation(0)
+            self.instrument.frequency_modulation('off')
+
         frequency = frequencies[0]
-        deviation = deviations[0]
         power = powers[0]
         self.instrument.frequency(frequency)
-        self.instrument.frequency_deviation(deviation)
         self.instrument.power(power)
 
         self.instrument.pulse_modulation('on')
         self.instrument.pulse_modulation_source('ext')
-
-        self.instrument.frequency_modulation('on')
-
-        self.instrument.frequency_modulation_source(self.modulation_channel())
         self.instrument.output_modulation('on')
 
     def start(self):
@@ -116,7 +123,7 @@ class SinePulseImplementation(PulseImplementation, SinePulse):
         return targeted_pulse
 
     def implement(self):
-        raise NotImplementedError('Sine pulse not yet implemented')
+        return {'frequency': self.frequency}
 
 
 class FrequencyRampPulseImplementation(PulseImplementation, FrequencyRampPulse):
