8d1ace41922330f85b6911e9cadf37642bf210be
NML <serwan.asaad@gmail.com>
2017-01-19 17:40:44 +1100

    Temporarily commenting out slack

--- experiments/EWJN/init/8_slack.py
+++ experiments/EWJN/init/8_slack.py
@@ -1,6 +1,6 @@
-slack = Slack()
+#slack = Slack()
 
-slack.commands['df_ds'] = DF_DS
-slack.commands['tgac'] = TGAC
-slack.commands['run'] = run_code
-slack.commands['halt'] = qc.halt_bg
+#slack.commands['df_ds'] = DF_DS
+#slack.commands['tgac'] = TGAC
+#slack.commands['run'] = run_code
+#slack.commands['halt'] = qc.halt_bg
