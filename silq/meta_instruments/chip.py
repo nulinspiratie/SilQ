a2318911512008a3f60ff6664d06a924918a2f2d
mtmadzik <mateusz.madzik@me.com>
2017-04-04 14:43:02 +1000

    Added channels to Chip meta-instrument

--- silq/meta_instruments/chip.py
+++ silq/meta_instruments/chip.py
@@ -1,5 +1,12 @@
 from qcodes import Instrument
+from qcodes.instrument.parameter import ManualParameter
+from qcodes.utils import validators as vals
+
 
 class Chip(Instrument):
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, channels, **kwargs):
         super().__init__(name, **kwargs)
+        self.add_parameter(name='channels',
+                           parameter_class=ManualParameter,
+                           initial_value=channels,
+                           vals=vals.Anything())
