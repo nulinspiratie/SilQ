5e887f5e6c4fbe1859ac43c75af4256a5a72a648
nulinspiratie <serwan.asaad@gmail.com>
2017-04-05 17:01:40 +1000

    Some changes to OF27

--- experiments/OF27/init/4_data.py
+++ experiments/OF27/init/4_data.py
@@ -2,7 +2,4 @@
 h5fmt = hdf5_format.HDF5Format()
 
 # Data handling
-qc.data.data_set.DataSet.default_io.base_location = \
-    config['user']['data_folder']
-loc_provider = qc.data.location.FormatLocation(fmt='{date}/#{counter}_{name}_{time}')
-qc.data.data_set.DataSet.location_provider=loc_provider
+qc.data.data_set.DataSet.default_io.base_location = config['user']['data_folder']
