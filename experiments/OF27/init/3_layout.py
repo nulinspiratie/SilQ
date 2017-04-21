86182a5ab3d3034268b08d9fa4ab4c2f0c65b2ac
NML <serwan.asaad@gmail.com>
2017-04-21 20:16:57 +1000

    added init file updates from pulse_signals

--- experiments/OF27/init/3_layout.py
+++ experiments/OF27/init/3_layout.py
@@ -2,8 +2,7 @@ from silq.meta_instruments.layout import Layout
 
 ### Layout and connectivity
 layout = Layout(name='layout',
-                instrument_interfaces=list(interfaces.values()),
-                server_name='layout_server' if USE_MP else None)
+                instrument_interfaces=list(interfaces.values()))
 
 layout.primary_instrument('pulseblaster')
 layout.acquisition_instrument('ATS')
