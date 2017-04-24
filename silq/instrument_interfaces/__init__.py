f38f0bb95932ac831a1e8eb3af6c1ca5b276b52d
Mark Johnson <mark.johnson.ai@gmail.com>
2017-04-24 13:55:58 +1000

    feat: add digitizer interface to list of interfaces.

--- silq/instrument_interfaces/__init__.py
+++ silq/instrument_interfaces/__init__.py
@@ -8,7 +8,7 @@ def get_instrument_interface(instrument):
     from .spincore.PulseBlasterESRPRO_interface import \
         PulseBlasterESRPROInterface
     from .AlazarTech.ATS_interface import ATSInterface
-    from .keysight import E8267DInterface
+    from .keysight import E8267DInterface, M3300A_DIGInterface
 
     instrument_interfaces = {
         'ArbStudio1104': ArbStudio1104Interface,
@@ -19,6 +19,7 @@ def get_instrument_interface(instrument):
         'ATS9440': ATSInterface,
         'MockATS': ATSInterface,
         'Keysight_E8267D': E8267DInterface
+        'Keysight_M3300A_DIG': M3300A_DIGInterface
     }
 
     instrument_class = get_instrument_class(instrument)
