c53c806c0c1a2cfc8dcf676d050ae62e1680ca7b
FQT <serwan.asaad@gmail.com>
2017-05-29 19:18:44 +1000

    Antimony: Added line magic and logger

--- experiments/Antimony/init/6_logging.py
+++ experiments/Antimony/init/6_logging.py
@@ -1,14 +1,16 @@
 ###############
 ### Logging ###
 ###############
-
+from silq.tools.general_tools import ParallelTimedRotatingFileHandler
 note_formatter = logging.Formatter('%(asctime)s - %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
 
-file_handler = logging.FileHandler(
-    os.path.join(config.properties.data_folder, r'notes\notes.log'))
-file_handler = logging.handlers.TimedRotatingFileHandler(
-    os.path.join(config.properties.data_folder, r'notes\notes.log'), when='midnight')
+# file_handler = logging.FileHandler(
+#     os.path.join(config.properties.data_folder, r'notes\notes.log'))
+# filename = os.path.join(config.properties.data_folder, r'notes\notes')
+filename = r'F:\Antimony\data\notes\notes'
+file_handler = ParallelTimedRotatingFileHandler(filename=filename,
+                                                when='midnight')
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(note_formatter)
 
