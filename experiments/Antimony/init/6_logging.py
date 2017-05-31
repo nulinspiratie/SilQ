775e0ee7c12c1e26b126741144e2b54faffb0067
FQT <serwan.asaad@gmail.com>
2017-05-31 13:49:57 +1000

    Antimony: Init added debug logger &  slack update

--- experiments/Antimony/init/6_logging.py
+++ experiments/Antimony/init/6_logging.py
@@ -2,12 +2,13 @@
 ### Logging ###
 ###############
 from silq.tools.general_tools import ParallelTimedRotatingFileHandler
+
+###################
+### Note logger ###
+###################
 note_formatter = logging.Formatter('%(asctime)s - %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
 
-# file_handler = logging.FileHandler(
-#     os.path.join(config.properties.data_folder, r'notes\notes.log'))
-# filename = os.path.join(config.properties.data_folder, r'notes\notes')
 filename = r'F:\Antimony\data\notes\notes'
 file_handler = ParallelTimedRotatingFileHandler(filename=filename,
                                                 when='midnight')
@@ -19,3 +20,37 @@ note_logger.setLevel(logging.INFO)
 
 note_logger.addHandler(file_handler)
 note_logger.addFilter(logging.Filter(name='notes'))
+
+
+##############
+### Logger ###
+##############
+
+log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - '
+                                  '%(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+
+filename = r'F:\Antimony\data\log\log'
+file_handler = ParallelTimedRotatingFileHandler(filename=filename,
+                                                when='midnight')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(log_formatter)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+if logger.handlers:
+    logger.handlers[0].setLevel(logging.INFO)
+logger.addHandler(file_handler)
+
+class logFilter:
+    def filter(self, record):
+        self.record = record
+        if record.name == 'requests.packages.urllib3.connectionpool':
+            # Filter these out, emitted by Slacker after every request
+            return False
+        else:
+            return True
+
+file_handler.addFilter(logFilter())
+logger.addFilter(logFilter())
--- experiments/Antimony/init/8_slack.py
+++ experiments/Antimony/init/8_slack.py
@@ -1,6 +1,7 @@
 import threading
 t = threading.Thread(target=Slack, name='slack',
-                     kwargs={'df_ds': DF_DS,
+                     kwargs={'auto_start': True,
+                             'df_ds': DF_DS,
                             'tgac': TGAC,
                             'run': run_code})
 t.start()
