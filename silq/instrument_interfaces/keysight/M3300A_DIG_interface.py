4a5f8ef6bcb8c46446a50a10f83d5c28316ea7a8
Mark Johnson <mark.johnson.ai@gmail.com>
2017-06-08 11:35:05 +1000

    refactor: simplify how duration of pulse_sequence is obtained

--- silq/instrument_interfaces/keysight/M3300A_DIG_interface.py
+++ silq/instrument_interfaces/keysight/M3300A_DIG_interface.py
@@ -217,13 +217,7 @@ class M3300A_DIG_Interface(InstrumentInterface):
             self.trigger_threshold(trigger_pulse.get_voltage(trigger_pulse.t_start) / 2)
             self.trigger_edge('rising')
 
-            t_start = min(pulse.t_start for pulse in
-                          self.input_pulse_sequence.get_pulses(acquire=True))
-            t_stop = max(pulse.t_stop for pulse in
-                         self.input_pulse_sequence.get_pulses(acquire=True))
-            t_final = max(self.input_pulse_sequence.t_stop_list)
-
-            T = t_stop - t_start
+            T = self.input_pulse_sequence.duration
 
             controller.sample_rate(int(round((self.sample_rate()))))
             controller.traces_per_acquisition(int(round(self.samples())))
