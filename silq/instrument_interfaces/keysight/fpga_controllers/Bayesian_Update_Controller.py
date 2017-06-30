6adb595738ec85b0b377dba0e6295599eb0b245c
Mark Johnson <mark.johnson.ai@gmail.com>
2017-06-30 17:30:59 +1000

    feature: add a Bayesian update FPGA controller and a basic SilQ interface for this controller.

---
+++ silq/instrument_interfaces/keysight/fpga_controllers/Bayesian_Update_Controller.py
@@ -0,0 +1,73 @@
+from qcodes.instrument_drivers.keysight.SD_common.SD_FPGA import SD_FPGA
+
+class Bayesian_Update_FPGA(SD_FPGA):
+    """
+            This class makes use of a Bayesian update module onboard the FPGA
+            of a Keysight digitizer board.
+        """
+
+    def __init__(self, name, chassis, slot, **kwargs):
+        super().__init__(name, chassis, slot, **kwargs)
+
+        self.write_port_signals = {
+            "reset":            {"address": 0x0, 'width': 1},
+            "enable":           {"address": 0x1, 'width': 1},
+            "blip_threshold":   {"address": 0x2, 'width': 1},
+            "blip_timeout":     {"address": 0x3, 'width': 1},
+            "trace_sel":        {"address": 0x4, 'width': 1},
+        }
+
+        self.read_port_signals = {
+            "counter": {"address": 0, 'width': 1},
+        }
+
+        self.add_parameter(
+            'blip_threshold',
+            set_cmd=self._set_blip_threshold,
+            docstring='The blip threshold in range [-0x8000, 0x7FFF].'
+        )
+        self.add_parameter(
+            'blip_timeout',
+            set_cmd=self._set_blip_timeout,
+            docstring='The blip timeout in samples.'
+        )
+        self.add_parameter(
+            'trace_select',
+            set_cmd=self._set_trace_select,
+            docstring='A mask for the channel trace you want to process. '
+                      'Bit 0 is channel 0, bit 1 is channel 1, etc.'
+        )
+
+    def _set_blip_threshold(self, threshold):
+        """ Sets the blip threshold to reset the Bayesian update timer. """
+        self.set_fpga_pc_port(0, [threshold],
+                              self.write_port_signals['blip_threshold']['address'], 1, 1)
+
+    def _set_blip_timeout(self, timeout):
+        """ Sets the blip timeout for the trigger of the Bayesian update module. """
+        self.set_fpga_pc_port(0, [timeout],
+                              self.write_port_signals['blip_timeout']['address'], 1, 1)
+
+    def _set_trace_select(self, trace_sel):
+        self.set_fpga_pc_port(0, [trace_sel],
+                              self.write_port_signals['trace_sel']['address'], 1, 1)
+
+    def _get_counter_value(self):
+        return self.get_fpga_pc_port(0, 1,
+                                     self.read_port_signals['counter']['address'], 1, 1)
+
+    def start(self):
+        """ Starts the currently loaded FPGA firmware. """
+        # Also pulls module out of reset
+        self.set_fpga_pc_port(0, [0, 1],
+                              self.write_port_signals['reset']['address'], 1, 1)
+
+    def stop(self):
+        """ Stops the currently loaded FPGA firmware. """
+        self.set_fpga_pc_port(0, [0],
+                              self.write_port_signals['enable']['address'], 1, 1)
+
+    def reset(self):
+        """ Resets the currently loaded FPGA firmware. """
+        self.set_fpga_pc_port(0, [1],
+                              self.write_port_signals['reset']['address'], 1, 1)
