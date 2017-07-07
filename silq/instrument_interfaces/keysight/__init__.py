139e0e5bc19eda061e461635d65a1cdf7dbd00be
Mark Johnson <mark.johnson.ai@gmail.com>
2017-07-07 16:04:14 +1000

    fix: add instrument interface to __init__.py

--- silq/instrument_interfaces/keysight/__init__.py
+++ silq/instrument_interfaces/keysight/__init__.py
@@ -1,3 +1,4 @@
 from .E8267D_interface import E8267DInterface
 from .M3201A_interface import M3201AInterface
 from .M3300A_DIG_interface import M3300A_DIG_Interface
+from .Keysight_FPGA_Interface import Bayesian_Update_Interface
