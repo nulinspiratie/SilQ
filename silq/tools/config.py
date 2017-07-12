d0ac75d1e6e69b7d42ccd017e86647b2647fbc90
nulinspiratie <serwan.asaad@gmail.com>
2017-07-12 10:25:45 +1000

    fix: config.get now never raises KeyError

--- silq/tools/config.py
+++ silq/tools/config.py
@@ -194,7 +194,7 @@ class DictConfig(SubConfig, DotDict):
             # print(f'cfg: sending {(key, val)} to {self.config_path}')
             signal(self.config_path).send(self, **{key: get_val})
 
-    def get(self, key, **kwargs):
+    def get(self, key, default=None):
         """
         Override dictionary get, because it otherwise does not call __getitem__
         Args:
@@ -207,10 +207,7 @@ class DictConfig(SubConfig, DotDict):
         try:
             return self[key]
         except KeyError:
-            if 'default' in kwargs:
-                return kwargs['default']
-            else:
-                raise
+            return None
 
 
     def _handle_config_signal(self, dependent_attr,  listen_attr, _, **kwargs):
