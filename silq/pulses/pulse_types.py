4b2f852ba0f340780a3298db52d747886192836a
nulinspiratie <serwan.asaad@gmail.com>
2017-09-07 11:09:02 +1000

    move Pulse.copy to Pulse.__copy__, include __deepcopy__

--- silq/pulses/pulse_types.py
+++ silq/pulses/pulse_types.py
@@ -376,6 +376,29 @@ class Pulse(HasTraits):
         name = 'CombinationPulse_{}'.format(id(self)+id(other))
         return CombinationPulse(name, self, other, '*')
 
+    def __copy__(self, *args):
+        """
+        Creates a copy of a pulse.
+        Args:
+
+        Returns:
+            Copy of pulse
+        """
+        # temporarily remove signal because it takes time copying
+        pulse_copy = copy.deepcopy(self)
+
+        # Add receiver for config signals
+        if hasattr(self, 'environment'):
+            # For PulseImplementation
+            signal(f'config:{pulse_copy.environment}.pulses.'
+                   f'{pulse_copy.name}').connect(
+                pulse_copy._handle_config_signal)
+            signal(f'config:{pulse_copy.environment}.properties').connect(
+                pulse_copy._handle_properties_config_signal)
+        return pulse_copy
+
+    __deepcopy__ = __copy__
+
     def _JSONEncoder(self):
         """
         Converts to JSON encoder for saving metadata
@@ -442,27 +465,6 @@ class Pulse(HasTraits):
 
         pulse_class = self.__class__.__name__
         return f'{pulse_class}({self.full_name}, {properties_str})'
-
-    def copy(self):
-        """
-        Creates a copy of a pulse.
-        Args:
-
-        Returns:
-            Copy of pulse
-        """
-        # temporarily remove signal because it takes time copying
-        pulse_copy = copy.deepcopy(self)
-
-        # Add receiver for config signals
-        if hasattr(self, 'environment'):
-            # For PulseImplementation
-            signal(f'config:{pulse_copy.environment}.pulses.'
-                   f'{pulse_copy.name}').connect(
-                pulse_copy._handle_config_signal)
-            signal(f'config:{pulse_copy.environment}.properties').connect(
-                pulse_copy._handle_properties_config_signal)
-        return pulse_copy
 
     def satisfies_conditions(self, pulse_class=None, name=None, **kwargs):
         """
