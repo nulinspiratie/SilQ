346e302fd8bc2322c4f6600c9707c10f35ecb81d
nulinspiratie <serwan.asaad@gmail.com>
2017-12-14 13:25:04 +1100

    doc: add napoleon

--- documentation/conf.py
+++ documentation/conf.py
@@ -21,7 +21,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import juliadoc
+# import juliadoc
 
 # -- General configuration ------------------------------------------------
 
@@ -41,6 +41,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
     'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -93,9 +94,10 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'julia'
-html_theme_path = [juliadoc.get_theme_dir()]
-html_sidebars = juliadoc.default_sidebars()
+html_theme = 'alabaster'
+# html_theme = 'julia'
+# html_theme_path = [juliadoc.get_theme_dir()]
+# html_sidebars = juliadoc.default_sidebars()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
