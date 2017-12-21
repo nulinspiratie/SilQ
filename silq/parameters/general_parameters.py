3841a450c5de4d0dc73a25e650fa0f322430da59
nulinspiratie <serwan.asaad@gmail.com>
2017-12-21 16:48:31 +1100

    doc: general parameters

--- silq/parameters/general_parameters.py
+++ silq/parameters/general_parameters.py
@@ -1,3 +1,4 @@
+from typing import List
 from time import sleep
 import numpy as np
 
@@ -11,18 +12,46 @@ from silq.tools import data_tools
 
 properties_config = config.get('properties', {})
 pulse_config = config.get('pulses', {})
-__all__ = ['CombinedParameter', 'ScaledParameter', 'StoreParameter',
-           'AttributeParameter', 'ConfigPulseAttribute']
+__all__ = ['CombinedParameter', 'AttributeParameter']
 
 
 class CombinedParameter(Parameter):
+    """Combines multiple parameters into a single parameter.
+
+
+    Setting this parameter sets all underlying parameters to this value, after
+    applying possible scale and offset in that order.
+    Getting this parameter gets the value of the first parameter, and applies
+    offset and scale in that order..
+
+    Args:
+        parameters: Parameters to be combined.
+        name: Name of `CombinedParameter`, by default equal to the names of the
+            composed parameters seperated by underscores.
+        label: Label of `CombinedParameter, by default equal to the labels of
+            the composed parameters separated by `` and ``. Also includes any
+            scale and offset.
+        unit: Parameter unit.
+        offsets: Optional offset for parameters. If set, must have equal number
+            of elements as parameters
+        scales: Optional scale for parameters. If set, must have equal number
+            of elements as parameters.
+        **kwargs: Additional kwargs passed to `Parameter`.
+
+    Note:
+        * All args are also attributes.
+        * While QCoDeS already has a `CombinedParameter`, it has some
+          shortcomings which are addressed here. Maybe in the future this will
+          be PR'ed to the main QCoDeS repository.
     """
-    Combines multiple parameters into a single parameter.
-    Setting this parameter sets all underlying parameters to this value
-    Getting this parameter gets the value of the first parameter
-    """
-    def __init__(self, parameters, name=None, label='', unit=None, offsets=None,
-                 scales=None, **kwargs):
+    def __init__(self,
+                 parameters: List[Parameter],
+                 name: str = None,
+                 label: str = '',
+                 unit: str = None,
+                 offsets: List[float] = None,
+                 scales: List[float] = None,
+                 **kwargs):
         if name is None:
             name = '_'.join([parameter.name for parameter in parameters])
 
@@ -61,11 +90,12 @@ class CombinedParameter(Parameter):
         self._label = label
 
     def zero_offset(self):
+        """Use current values of parameters as offsets."""
         self.offsets = [param() for param in self.parameters]
 
     def calculate_individual_values(self, value):
-        """
-        Calulate values of parameters from a combined value
+        """Calulate values of parameters from a combined value
+
         Args:
             value: combined value
 
@@ -98,141 +128,38 @@ class CombinedParameter(Parameter):
             sleep(0.005)
 
 
-class ScaledParameter(Parameter):
-    """
-    Creates a new parameter that scales a previous parameter by a ratio
-    Setting this parameter sets the underlying parameter multiplied by the ratio
-    Getting this parameter gets the underlying parameter divided by the ratio
-    """
-    def __init__(self, parameter, scale=1,
-                 name=None, label=None, unit=None, **kwargs):
-        if name is None:
-            name = parameter.name
-        if label is None:
-            label = parameter.label
-        if unit is None:
-            unit = parameter.unit
-        super().__init__(name, label=label, unit=unit, **kwargs)
-        self.parameter = parameter
-
-        self.scale = scale
-        self._meta_attrs.extend(['scale'])
-
-    def get_raw(self):
-        value = self.parameter() / self.scale
-        self._save_val(value)
-        return value
-
-    def set_raw(self, val):
-        value = val * self.scale
-        self._save_val(val)
-        self.parameter(value)
-
-
-class StoreParameter(Parameter):
-    """
-    Stores data in a separate file (not working anymore, needs upgrading)
-    """
-    def __init__(self, shape, data_manager, formatter=None, **kwargs):
-        super().__init__(name='random_parameter', shape=shape, **kwargs)
-        self.data_manager = data_manager
-        self.setup(formatter=formatter)
-
-    def setup(self, formatter=None):
-        data_array_set = DataArray(name='set_vals',
-                                   shape=(self.shape[0],),
-                                   preset_data=np.arange(self.shape[0]),
-                                   is_setpoint=True)
-        index0 = DataArray(name='index0', shape=self.shape,
-                           preset_data=np.full(self.shape,
-                                               np.arange(self.shape[-1]),
-                                               dtype=np.int))
-        data_array_values = DataArray(name='data_vals',
-                                      shape=self.shape,
-                                      set_arrays=(
-                                      data_array_set, index0))
-
-        data_folder = data_tools.get_data_folder()
-        loc_provider = qc.data.location.FormatLocation(
-            fmt=data_folder+'/traces/#{counter}_trace_{time}')
-
-        self.data_set = new_data(
-            location=loc_provider,
-            arrays=[data_array_set, index0, data_array_values],
-            name='test_data_parameter',
-            formatter=formatter)
-
-    def get_raw(self):
-        result = np.random.randint(1, 100, size=self.shape)
-
-        loop_indices = slice(0,self.shape[0],1)
-        ids_values = {'data_vals': result}
-                      # 'set_vals_set': np.arange(self.shape[0]),
-                      # 'index0': None}
-        self.data_manager.write('store_data', loop_indices, ids_values)
-        self.data_manager.write('finalize_data')
-        return result
-
-
 class AttributeParameter(Parameter):
-    def __init__(self, object, attribute, name=None, scale=None, is_key=None,
+    """Creates a parameter that can set/get an attribute from an object.
+
+    Args:
+        object: Object whose attribute to set/get.
+        attribute: Attribute to set/get
+        is_key: whether the attribute is a key in a dictionary. If not
+            specified, it will check if `AttributeParameter.object` is a dict.
+        **kwargs: Additional kwargs passed to `Parameter`.
+    """
+    def __init__(self,
+                 object: object,
+                 attribute: str,
+                 name: str = None,
+                 is_key: bool = None,
                  **kwargs):
-        """
-        Creates a parameter that can set/get an attribute from an object
-        Args:
-            object: object whose attribute to set/get
-            attribute: attribute to set/get
-            is_key: whether the attribute is a key in a dictionary
-            **kwargs: Other parameter kwargs
-        """
         name = name if name is not None else attribute
         super().__init__(name=name, **kwargs)
 
         self.object = object
         self.attribute = attribute
-        self.scale = scale
         self.is_key = isinstance(object, dict) if is_key is None else is_key
 
     def set_raw(self, value):
-        if self.scale is not None:
-            value = tuple(value / scale for scale in self.scale)
         if not self.is_key:
             setattr(self.object, self.attribute, value)
         else:
             self.object[self.attribute] = value
-        self._save_val(value)
 
     def get_raw(self):
         if not self.is_key:
             value =  getattr(self.object, self.attribute)
         else:
             value = self.object[self.attribute]
-        if self.scale is not None:
-            value = value[0] * self.scale[0]
-        self._save_val(value)
-        return value
-
-
-class ConfigPulseAttribute(Parameter):
-    def __init__(self, pulse_name, attribute, **kwargs):
-        """
-        Creates a parameter that can set/get an attribute from an object
-        Args:
-            pulse_name: name of pulse in config['pulses'] keys
-            attribute: attribute to set/get
-            **kwargs: Other parameter kwargs
-        """
-        name = kwargs.pop('name', '{}_{}'.format(pulse_name, attribute))
-        super().__init__(name=name, **kwargs)
-
-        self.pulse_name = pulse_name
-        self.attribute = attribute
-
-    def set_raw(self, value):
-        pulse_config[self.pulse_name][self.attribute] = value
-        self._save_val(value)
-
-    def get_raw(self):
-        value = pulse_config[self.pulse_name][self.attribute]
-        self._save_val(value)
         return value
