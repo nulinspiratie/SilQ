43e2754f7d1d7aeabbe1aff78a02f51ef32ba167
nulinspiratie <serwan.asaad@gmail.com>
2018-01-07 19:56:50 +1100

    Add Guidelines for instrumentinterface

--- documentation/conf.py
+++ documentation/conf.py
@@ -209,6 +209,8 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/': None,
+                       'http://qcodes.github.io/Qcodes/': None}
+                       # 'qcodes': ('http://qcodes.github.io/Qcodes/', None)}
 
 default_role = 'any'
