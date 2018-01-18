56e5090bd5593552717e94b1edbea5e717e3c6d2
nulinspiratie <serwan.asaad@gmail.com>
2018-01-19 10:49:23 +1100

    add get_additional_pulses

--- silq/instrument_interfaces/Tektronix/AWG520_interface.py
+++ silq/instrument_interfaces/Tektronix/AWG520_interface.py
@@ -47,25 +47,80 @@ class AWG520Interface(InstrumentInterface):
                            initial_value=.1e-6,
                            doc='Time subtracted from each waveform to ensure '
                                'that it is finished once next trigger arrives.')
+        self.add_parameter('trigger_in_duration',
+                           unit='s',
+                           set_cmd=None,
+                           initial_value=.1e-6)
         self.add_parameter('active_channels',
                            set_cmd=None,
                            vals=Lists(Enum(1,2)))
 
     def get_additional_pulses(self, **kwargs):
-        additional_pulses = []
-
         # Return empty list if no pulses are in the pulse sequence
         if not self.pulse_sequence or self.is_primary():
-            return additional_pulses
+            return []
+
+        active_channels = {pulse.connection.output['channel']
+                           for pulse in self.pulse_sequence}
+
+        # Add DCPulse(amplitude=0) for any gaps between pulses
+        gap_pulses = []
+        for channel in active_channels:
+            pulses = sorted(self.pulse_sequence.get_pulses(output_channel=channel),
+                            key=lambda pulse: pulse.t_start)
+            t = 0
+            for pulse in pulses:
+                if pulse.t_start < t:
+                    raise RuntimeError(f'Pulse {pulse} starts before previous'
+                                       f'pulse is finished')
+                elif pulse.t_start > t:
+                    # Add DCPulse to fill gap between pulses
+                    gap_pulse = DCPulse(t_start=t,
+                                        t_stop=pulse.t_start,
+                                        amplitude=0)
+                    gap_pulses.append(self.get_pulse_implementation(gap_pulse))
+                t = pulse.t_start
+
+            if t < self.pulse_sequence.duration:
+                # Add DCPulse to fill gap between pulses
+                gap_pulse = DCPulse(t_start=t,
+                                    t_stop=self.pulse_sequence.duration,
+                                    amplitude=0)
+                gap_pulses.append(self.get_pulse_implementation(gap_pulse))
+
+        self.pulse_sequence.add(*gap_pulses)
+
+        # If a pulse starts on one channel and needs a trigger while another
+        # pulse is still active on the other channel, this would cause the other
+        # pulse to move onto the next pulse prematurely. This only happens if
+        # the other pulse finished its waveform and is waiting for a trigger.
+        # Here we check that this does not occur.
+        if len(active_channels) > 1:
+            for t_start in self.pulse_sequence.t_start_list:
+                if any(pulse.t_start < t_start < pulse.t_stop
+                       and not pulse.implementation.is_full_waveform
+                       for pulse in self.pulse_sequence):
+                    raise RuntimeError(f'At t={t_start} s, a new pulse starts'
+                                       f'while another pulse is still active.')
+                    #TODO Implement splitting of pulses
 
         # TODO test if first waveform needs trigger as well
+        additional_pulses = [
+            TriggerPulse(t_start=t_start,
+                         duration=self.trigger_in_duration(),
+                         connection_requirements={
+                             'input_instrument': self.instrument_name(),
+                             'trigger': True})
+            for t_start in self.pulse_sequence.t_start_list
+        ]
 
-
-
+        return additional_pulses
 
     def setup(self, is_primary=False, **kwargs):
         if is_primary:
-            logger.warning('AWG520 cannot function as primary instrument')
+            raise RuntimeError('AWG520 cannot function as primary instrument')
+
+        
 
     def start(self):
         self.instrument.start()
