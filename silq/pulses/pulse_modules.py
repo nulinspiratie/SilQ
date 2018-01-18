5b9438281693cc5d26a97ae16f7c7072259a78b9
nulinspiratie <serwan.asaad@gmail.com>
2018-01-19 10:49:46 +1100

    fix: make sure t_start_list is sorted and unique

--- silq/pulses/pulse_modules.py
+++ silq/pulses/pulse_modules.py
@@ -243,16 +243,15 @@ class PulseSequence:
 
     @property
     def t_start_list(self):
-        return [pulse.t_start for pulse in self.enabled_pulses]
+        return sorted({pulse.t_start for pulse in self.enabled_pulses})
 
     @property
     def t_stop_list(self):
-
-        return [pulse.t_stop for pulse in self.enabled_pulses]
+        return sorted({pulse.t_stop for pulse in self.enabled_pulses})
 
     @property
     def t_list(self):
-        return list(set(self.t_start_list + self.t_stop_list + [self.duration]))
+        return sorted({self.t_start_list + self.t_stop_list + [self.duration]})
 
     def replace(self, pulse_sequence):
         """
