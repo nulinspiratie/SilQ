bcb59f436c9c2035b1ddd140c1b35653d451832d
nulinspiratie <serwan.asaad@gmail.com>
2018-01-19 15:24:35 +1100

    more bugfixes

--- silq/tests/test_interfaces/test_AWG520_interface.py
+++ silq/tests/test_interfaces/test_AWG520_interface.py
@@ -41,6 +41,8 @@ def setup():
     layout.load_connections(connections_dicts=[
         {"output_arg": "AWG520.ch1",
          "input_arg": "chip.ch1"},
+        {"output_arg": "AWG520.ch2",
+         "input_arg": "chip.ch2"},
         {"output_arg": "triggerer.ch1",
          "input_arg": "AWG520.trig_in",
          "trigger": True},
@@ -64,4 +66,57 @@ def test_single_DC_pulse(setup):
 
     layout.pulse_sequence = pulse_sequence
 
-    print(repr(AWG_interface.pulse_sequence))
+def test_gap_pulse(setup):
+    layout = setup['layout']
+    AWG_interface = setup['AWG_interface']
+    trigger_interface = setup['trigger_interface']
+
+    DC_pulse1 = DCPulse(t_start=0, duration=10e-3, amplitude=1,
+                       connection_requirements={'output_arg': 'AWG520.ch1'})
+    DC_pulse2 = DCPulse(t_start=20e-3, duration=10e-3, amplitude=1,
+                       connection_requirements={'output_arg': 'AWG520.ch1'})
+    pulse_sequence = PulseSequence(pulses=[DC_pulse1, DC_pulse2])
+
+    layout.pulse_sequence = pulse_sequence
+    assert len(AWG_interface.pulse_sequence) == 3
+    assert len(trigger_interface.pulse_sequence) == 3
+
+    assert AWG_interface.pulse_sequence.get_pulse(t_start=10e-3, t_stop=20e-3,
+                                                  amplitude=0)
+
+
+def test_double_channels(setup):
+    layout = setup['layout']
+    AWG_interface = setup['AWG_interface']
+    trigger_interface = setup['trigger_interface']
+
+    DC_pulse1 = DCPulse(t_start=0, duration=10e-3, amplitude=1,
+                       connection_requirements={'output_arg': 'AWG520.ch1'})
+    DC_pulse2 = DCPulse(t_start=0e-3, duration=10e-3, amplitude=1,
+                       connection_requirements={'output_arg': 'AWG520.ch2'})
+    pulse_sequence = PulseSequence(pulses=[DC_pulse1, DC_pulse2])
+
+    layout.pulse_sequence = pulse_sequence
+    assert len(AWG_interface.pulse_sequence) == 2
+    assert len(trigger_interface.pulse_sequence) == 1
+
+    assert len(AWG_interface.pulse_sequence.get_pulses(t_start=0e-3,
+                                                       t_stop=10e-3)) == 2
+
+def test_double_channels_different_t_start(setup):
+    layout = setup['layout']
+    AWG_interface = setup['AWG_interface']
+    trigger_interface = setup['trigger_interface']
+
+    DC_pulse1 = DCPulse(t_start=0, duration=10e-3, amplitude=1,
+                       connection_requirements={'output_arg': 'AWG520.ch1'})
+    DC_pulse2 = DCPulse(t_start=20e-3, duration=10e-3, amplitude=1,
+                       connection_requirements={'output_arg': 'AWG520.ch2'})
+    pulse_sequence = PulseSequence(pulses=[DC_pulse1, DC_pulse2])
+
+    layout.pulse_sequence = pulse_sequence
+    assert len(AWG_interface.pulse_sequence) == 6
+    assert len(trigger_interface.pulse_sequence) == 3
+
+    assert len(AWG_interface.pulse_sequence.get_pulses(t_start=0e-3,
+                                                       t_stop=10e-3)) == 2
