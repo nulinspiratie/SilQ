2c068b0c833360c6db99bb6a0488c52a8938ecfb
nulinspiratie <serwan.asaad@gmail.com>
2018-05-03 21:10:49 +1000

    feat: allow_none for pulse.id

--- silq/tests/test_pulses.py
+++ silq/tests/test_pulses.py
@@ -1,14 +1,12 @@
 import unittest
-import tempfile
 from copy import copy, deepcopy
 
-from silq.pulses import PulseSequence, DCPulse, TriggerPulse, Pulse
-from silq.instrument_interfaces import Channel
-from silq.meta_instruments.layout import SingleConnection
+from silq.pulses import DCPulse, Pulse
 from silq.tools.config import *
 import silq
 
 import qcodes as qc
+
 
 class TestPulse(unittest.TestCase):
     def test_pulse_equality(self):
@@ -36,6 +34,10 @@ class TestPulse(unittest.TestCase):
 
         p.duration = 4
         self.assertEqual(p.t_stop, 5)
+
+    def test_pulse_no_id(self):
+        p = Pulse('name')
+        p.id = None
 
 
 class TestPulseSignals(unittest.TestCase):
@@ -130,135 +132,6 @@ class TestPulseConfig(unittest.TestCase):
         self.config.env1.pulses.read.t_stop = 10
         self.assertEqual(p.t_start, 5)
         self.assertEqual(p.t_stop, 10)
-
-#
-# class TestPulseSequence(unittest.TestCase):
-#     def setUp(self):
-#
-#         config.clear()
-#         config.properties = {}
-#
-#         self.pulse_sequence = PulseSequence()
-#
-#     def test_add_remove_pulse(self):
-#         if self.pulse_sequence:
-#             isempty = False
-#         else:
-#             isempty = True
-#         self.assertTrue(isempty)
-#
-#         pulse = DCPulse(name='dc', amplitude=1.5, duration=10, t_start=0)
-#         self.pulse_sequence.add(pulse)
-#         self.assertIn(pulse, self.pulse_sequence)
-#
-#         if self.pulse_sequence:
-#             isempty = False
-#         else:
-#             isempty = True
-#         self.assertFalse(isempty)
-#
-#         # Remove pulses
-#         self.pulse_sequence.clear()
-#         self.assertEqual(len(self.pulse_sequence.pulses), 0)
-#
-#         if self.pulse_sequence:
-#             isempty = False
-#         else:
-#             isempty = True
-#         self.assertTrue(isempty)
-#
-#     def test_sort(self):
-#         pulse1 = DCPulse(name='dc1', amplitude=1.5, duration=10, t_start=1)
-#         pulse2 = DCPulse(name='dc2', amplitude=1.5, duration=10, t_start=0)
-#         self.pulse_sequence.add(pulse1, pulse2)
-#
-#         self.assertEqual(pulse2, self.pulse_sequence[0])
-#
-#     def test_get_pulses(self):
-#         self.assertListEqual(self.pulse_sequence.get_pulses(), [])
-#         pulse1 = DCPulse(name='dc1', amplitude=1.5, duration=10, t_start=1)
-#         pulse2 = DCPulse(name='dc2', amplitude=2.5, duration=10, t_start=1)
-#         pulse3 = TriggerPulse(name='trig', duration=12, t_start=1)
-#         self.pulse_sequence.add(pulse1, pulse2, pulse3)
-#
-#         subset_pulses = self.pulse_sequence.get_pulses()
-#         self.assertListEqual(subset_pulses, [pulse1, pulse2, pulse3])
-#         subset_pulses = self.pulse_sequence.get_pulses(t_start=1)
-#         self.assertListEqual(subset_pulses, [pulse1, pulse2, pulse3])
-#         subset_pulses = self.pulse_sequence.get_pulses(duration=10)
-#         self.assertListEqual(subset_pulses, [pulse1, pulse2])
-#         subset_pulses = self.pulse_sequence.get_pulses(amplitude=1.5)
-#         self.assertListEqual(subset_pulses, [pulse1])
-#         subset_pulses = self.pulse_sequence.get_pulses(amplitude=('>', 1.5))
-#         self.assertListEqual(subset_pulses, [pulse2])
-#         subset_pulses = self.pulse_sequence.get_pulses(amplitude=('>=', 1.5))
-#         self.assertListEqual(subset_pulses, [pulse1, pulse2])
-#
-#         pulse = self.pulse_sequence.get_pulse(amplitude=1.5)
-#         self.assertEqual(pulse, pulse1)
-#         pulse = self.pulse_sequence.get_pulse(duration=12)
-#         self.assertEqual(pulse, pulse3)
-#         with self.assertRaises(RuntimeError):
-#             self.pulse_sequence.get_pulse(duration=10)
-#
-#     def test_transition_voltages(self):
-#         # To test transitions, pulses must be on the same connection
-#         channel_out = Channel('arbstudio', 'ch1', id=1, output=True)
-#         channel_in = Channel('device', 'input', id=1, output=True)
-#         c1 = SingleConnection(output_instrument='arbstudio',
-#                               output_channel=channel_out,
-#                               input_instrument='device',
-#                               input_channel=channel_in)
-#         pulses = [DCPulse(name='dc1', amplitude=0, duration=5, t_start=0,
-#                           connection=c1),
-#                   DCPulse(name='dc2', amplitude=1, duration=10, t_start=5,
-#                           connection=c1),
-#                   DCPulse(name='dc3', amplitude=2, duration=8, t_start=15,
-#                           connection=c1),
-#                   DCPulse(name='dc4', amplitude=3, duration=7, t_start=12,
-#                           connection=c1)]
-#
-#         self.pulse_sequence.add(*pulses)
-#
-#         self.assertRaises(TypeError, self.pulse_sequence.get_transition_voltages)
-#         self.assertRaises(TypeError, self.pulse_sequence.get_transition_voltages,
-#                           connection=c1)
-#         self.assertRaises(TypeError, self.pulse_sequence.get_transition_voltages,
-#                           t=5)
-#
-#         transition_voltage = self.pulse_sequence.get_transition_voltages(
-#             pulse=pulses[1])
-#         self.assertTupleEqual(transition_voltage, (0, 1))
-#
-#         transition_voltage = self.pulse_sequence.get_transition_voltages(
-#             connection=c1, t=5)
-#         self.assertTupleEqual(transition_voltage, (0, 1))
-#
-#         transition_voltage = self.pulse_sequence.get_transition_voltages(
-#             connection=c1, t=15)
-#         self.assertTupleEqual(transition_voltage, (1, 2))
-#
-#     def test_pulse_sequence_id(self):
-#         self.pulse_sequence.add(Pulse(name='read', duration=1))
-#         p1_read = self.pulse_sequence['read']
-#         self.assertIsNone(p1_read.id)
-#
-#         self.pulse_sequence.add(Pulse(name='load', duration=1))
-#         self.assertIsNone(p1_read.id)
-#
-#         self.pulse_sequence.add(Pulse(name='read', duration=1))
-#         self.assertEqual(p1_read.id, 0)
-#         self.assertEqual(self.pulse_sequence.get_pulse(name='read', id=0),
-#                          p1_read)
-#         self.assertEqual(self.pulse_sequence.get_pulse(name='read[0]'),
-#                          p1_read)
-#         p2_read = self.pulse_sequence['read[1]']
-#         self.assertNotEqual(p2_read, p1_read)
-#
-#         self.pulse_sequence.add(Pulse(name='read', duration=1))
-#         p3_read = self.pulse_sequence['read[2]']
-#         self.assertNotEqual(p3_read, p1_read)
-#         self.assertNotEqual(p3_read, p2_read)
 
 
 if __name__ == '__main__':
