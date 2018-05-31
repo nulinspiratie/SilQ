a20b9f2544837eacd2188b1c15a6b8da22a5e51b
Serwan Asaad <serwan.asaad@gmail.com>
2018-05-31 17:56:04 +1000

    fix: typo in sd_dig_interface

--- silq/instrument_interfaces/keysight/__init__.py
+++ silq/instrument_interfaces/keysight/__init__.py
@@ -1,3 +1,3 @@
 from .E8267D_interface import E8267DInterface
 from .Keysight_SD_AWG_interface import Keysight_SD_AWG_Interface
-from .Keysight_SD_DIG_Interface import Keysight_SD_DIG_Interface
+from .Keysight_SD_DIG_interface import Keysight_SD_DIG_Interface
