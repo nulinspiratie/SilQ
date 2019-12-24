89943d04c74c69e38243e57ee7a807fa00008b14
nulinspiratie <serwan.asaad@gmail.com>
2019-12-24 13:09:32 +1100

    add general_tools function to dtermine length of slice

--- silq/tools/general_tools.py
+++ silq/tools/general_tools.py
@@ -715,3 +715,13 @@ def logclass(cls, methodsAsFunctions=False,
                 logmethod(value.__func__, log = log, displayName=cls.__name__)))
 
     return cls
+
+
+def slice_length(s, length):
+    if isinstance(s, slice):
+        start = s.start if s.start is not None else 0
+        stop = s.stop if s.stop is not None else length
+        step = s.step if s.step is not None else 1
+        return int((stop - start) / step)
+    else:
+        return 1
