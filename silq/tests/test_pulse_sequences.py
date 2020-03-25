a19f4a260da1c1d4962a781a17524bab772558ac
Serwan Asaad <serwan.asaad@gmail.com>
2020-03-26 09:16:33 +1100

    remove test that ensures pulses can't be created without duration (they can!)

--- silq/tests/test_pulse_sequences.py
+++ silq/tests/test_pulse_sequences.py
@@ -69,11 +69,6 @@ class TestPulseSequence(unittest.TestCase):
         self.assertNotEqual(p3_read, p1_read)
         self.assertNotEqual(p3_read, p2_read)
 
-    def test_pulse_no_duration_error(self):
-        p = Pulse()
-        with self.assertRaises(AssertionError):
-            PulseSequence([p])
-
     def test_get_pulses(self):
         pulse_sequence = PulseSequence()
         self.assertListEqual(pulse_sequence.get_pulses(), [])
