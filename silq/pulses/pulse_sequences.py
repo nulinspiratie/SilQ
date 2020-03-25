f44f11fa04f0c9abb35a68344aa4a91daf3679c4
Serwan Asaad <serwan.asaad@gmail.com>
2020-03-25 15:13:51 +1100

    allow custom pulses for ESRPulseSequenceComposite

--- silq/pulses/pulse_sequences.py
+++ silq/pulses/pulse_sequences.py
@@ -364,20 +364,23 @@ class ESRPulseSequenceComposite(PulseSequence):
         For given pulse settings, `ESRPulseSequence.generate` will recreate the
         pulse sequence from settings.
 """
-    def __init__(self, **kwargs):
+    def __init__(self, pulse_sequences=None, **kwargs):
         super().__init__(**kwargs)
 
-        self.pulse_sequences = [
-            ElectronReadoutPulseSequence(name='ESR'),
-            PulseSequence(
-                name='EPR',
-                pulses=[
-                    DCPulse('empty', acquire=True),
-                    DCPulse('plunge', acquire=True),
-                    DCPulse('read_long', acquire=True)
+        if pulse_sequences is None:
+            pulse_sequences = [
+                ElectronReadoutPulseSequence(name='ESR'),
+                PulseSequence(
+                    name='EPR',
+                    pulses=[
+                        DCPulse('empty', acquire=True),
+                        DCPulse('plunge', acquire=True),
+                        DCPulse('read_long', acquire=True)
+                    ]
+                )
                 ]
-            )
-        ]
+
+        self.pulse_sequences = pulse_sequences
 
     @property
     def ESR(self):
