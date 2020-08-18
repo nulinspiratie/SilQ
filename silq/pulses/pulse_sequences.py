e125934439b7dc5f2ce1e2d5b94a88ab1ff9254c
Serwan Asaad <serwan.asaad@gmail.com>
2020-08-18 15:03:18 +1000

    add CircuitPulseSequence

--- silq/pulses/pulse_sequences.py
+++ silq/pulses/pulse_sequences.py
@@ -1679,6 +1679,82 @@ class FlipFlopPulseSequence(PulseSequenceGenerator):
         self.add(*self.pulse_settings['post_pulses'])
 
 
+class CircuitPulseSequence(ElectronReadoutPulseSequence):
+    def __init__(self, name='circuit', circuits_file=None, **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.settings["pulses"] = {}
+
+        self.circuit_index = Parameter(vals=vals.Ints(), parent=False)
+        self.circuit = Parameter(vals=vals.Strings())
+        self.circuits = Parameter(
+            vals=vals.Lists(vals.Strings()), set_cmd=None, initial_value=[]
+        )
+
+        self.circuits_file = Parameter(initial_value=circuits_file, set_cmd=None)
+        self.circuits_folder = Parameter(
+            config_link='properties.circuits_folder', set_cmd=None
+        )
+
+    @parameter
+    def circuit_index_set(self, parameter, idx):
+        self.circuit = self.circuits[idx]
+
+    @parameter
+    def circuit_set(self, parameter, circuit):
+        # Update current value of circuit
+        parameter._latest["raw_value"] = parameter._latest["value"] = circuit
+
+        gates = convert_circuit(circuit, target_type=list)
+
+        unknown_gates = [gate for gate in gates if gate not in self.settings.pulses]
+        if unknown_gates:
+            raise RuntimeError(
+                f'The following pulses are not registered in '
+                f'CircuitPulseSequence.settings.pulses: {unknown_gates}'
+            )
+
+        self.settings.RF_pulses = [[self.settings.pulses[gate] for gate in gates]]
+
+        self.generate()
+
+    def load_circuits(self, filepath):
+        if not isinstance(filepath, Path):
+            filepath = Path(filepath)
+
+        if not filepath.exists():
+            if self.circuits_folder is None:
+                raise RuntimeError(f'Could not find filepath at {filepath}')
+
+            filepath = Path(self.circuits_folder) / filepath
+
+        if not filepath.exists():
+            raise RuntimeError(f'Could not find filepath at {filepath}')
+
+        self.circuits = load_circuits(filepath, target_type=str)
+
+        return self.circuits
+
+    def save_circuits(self, filepath, name='circuits.txt'):
+
+        if isinstance(filepath, Measurement):
+            filepath = filepath.dataset.filepath
+        elif isinstance(filepath, DataSet):
+            filepath = filepath.filepath
+
+        filepath = Path(filepath)
+
+        if filepath.is_dir():
+            filepath = filepath / name
+
+        with filepath.open('w') as f:
+            f.write('\n'.join(self.circuits))
+
+        return filepath
+
+    def save(self, filepath):
+        return self.save_circuits(filepath)
+
+
 class NMRCircuitPulseSequence(NMRPulseSequenceComposite):
     def __init__(self, name='NMR_circuit', circuits_file=None, **kwargs):
         super().__init__(name=name, **kwargs)
