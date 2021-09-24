d5608f923b754cea3d604c53a0222b5d64f2111b
Serwan <serwan.asaad@gmail.com>
2021-09-24 14:05:31 +0200

    Add support for decoupled pulses & instructions

--- silq/instrument_interfaces/keysight/fpga_interfaces/PCDDS_interface.py
+++ silq/instrument_interfaces/keysight/fpga_interfaces/PCDDS_interface.py
@@ -59,6 +59,11 @@ class PCDDSInterface(InstrumentInterface):
         )
 
     @property
+    def decoupled_mode(self):
+        from qcodes.instrument_drivers.CQC2T.PCDDS import PCDDS
+        return not isinstance(self.instrument, PCDDS)
+
+    @property
     def active_channel_ids(self):
         """Sorted list of active channel id's"""
         # First create a set to ensure unique elements
@@ -79,7 +84,7 @@ class PCDDSInterface(InstrumentInterface):
         assert self.use_trig_in(), "Interface not yet programmed for pxi triggering"
         if self.auto_advance():
             # Only trigger once at start of sequence
-            t_list = min(self.pulse_sequence.t_list)
+            t_list = [min(self.pulse_sequence.t_list)]
         else:
             # Get list of unique pulse start and stop times
             t_list = self.pulse_sequence.t_list
@@ -93,14 +98,108 @@ class PCDDSInterface(InstrumentInterface):
                           for t in t_list if t != self.pulse_sequence.duration]
         return trigger_pulses
 
-    def setup(self, **kwargs):
-        for channel in self.instrument.channels:
-            channel.instruction_sequence().clear()
-        self.instrument.channels.output_enable(False)
-        self.instrument.channels.pcdds_enable(True)
+    def setup_decoupled(self):
+        assert self.auto_advance(), "decoupled setup has only been programmed with auto_advance enabled"
+        PCDDS_pulses = []
+        PCDDS_instructions = []
 
-        assert self.use_trig_in(), "Interface not yet programmed for pxi triggering"
+        def add_pulse_and_instruction(pulse_implementation):
+            try:
+                pulse_idx = PCDDS_pulses.index(pulse_implementation)
+            except ValueError:
+                pulse_idx = len(PCDDS_pulses)
+                pulse_implementation['pulse_idx'] = pulse_idx
+                PCDDS_pulses.append(pulse_implementation)
 
+            # Add corresponding instruction
+            instruction_idx = len(PCDDS_instructions)
+            PCDDS_instructions.append({
+                'instruction_idx': instruction_idx,
+                'pulse_idx': pulse_idx,
+                'next_instruction': instruction_idx+1
+            })
+            return pulse_idx, instruction_idx
+
+        # First pulses are 0V DC pulses
+        # Only trigger on first pulse
+        # t_start and duration must be set but are irrelevant
+        DC_0V_pulse = self.get_pulse_implementation(
+            DCPulse('initial_0V', t_start=0, duration=0, amplitude=0)
+        )
+
+        current_pulses = {channel.name: DC_0V_pulse
+                          for channel in self.active_instrument_channels}
+
+        for channel in self.active_instrument_channels:
+            current_pulse = current_pulses[channel.name]
+            pulse_implementation = current_pulse.implementation.implement()
+            add_pulse_and_instruction(pulse_implementation)
+
+        # Use clock cycles for maximum accuracy
+        clk = self.instrument.ch1.clk
+        timing_offset = self.instrument.ch1.pulse_timing_offset
+
+        t_min = min(self.pulse_sequence.t_list)
+        cycles_min = int(round(t_min * clk))
+
+        for channel in self.active_instrument_channels:
+            cycles = cycles_min
+            channel_pulses = self.pulse_sequence.get_pulses(output_channel=channel.name)
+            for pulse in channel_pulses:
+                start_cycles = int(round(pulse.t_start * clk))
+                delta_cycles = start_cycles - cycles
+                if delta_cycles > timing_offset:
+                    # Add 0V pulse to bridge gap
+                    pulse_implementation = DC_0V_pulse.implementation.implement()
+                    pulse_implementation['duration'] = delta_cycles / clk
+                    add_pulse_and_instruction(pulse_implementation)
+
+                    # Increment counters
+                    cycles += delta_cycles
+                elif delta_cycles > 1:
+                    logger.warning(
+                        f'Pulse {pulse} starts too early: {start_cycles} vs {cycles}. '
+                        'Could mean the pulse has insufficient delay. '
+                        'Next pulse will have a longer duration to bridge gap'
+                    )
+                elif delta_cycles < -1:
+                    logger.warning(
+                        f'Pulse {pulse} starts too early: {start_cycles} vs {cycles}. '
+                        'This error should not occur unless pulses overlap. '
+                        'Next pulse will have shorter duration to bridge gap'
+                    )
+
+                stop_cycles = int(round(pulse.t_stop * clk))
+                delta_cycles = stop_cycles - cycles
+                if delta_cycles <= timing_offset:
+                    logger.warning(f'Pulse {pulse} is too short. '
+                                   f'{stop_cycles} vs {cycles} cycles')
+                    delta_cycles = 15
+
+                # Implement pulse
+                pulse_implementation = pulse.implementation.implement()
+                pulse_implementation['duration'] = delta_cycles / clk
+                add_pulse_and_instruction(pulse_implementation)
+
+                # Increment counters
+                cycles += delta_cycles
+
+            # Add a final DC pulse that requires triggering to restart
+            pulse_implementation = DC_0V_pulse.implementation.implement()
+            add_pulse_and_instruction(pulse_implementation)
+
+            PCDDS_instructions[-1]['next_instruction'] = 1
+            # TODO: Add check that final DC pulse is not added if last pulse
+            # ends at pulsesequence.duration, in which case the last pulse
+            # should be triggered
+
+            for pulse in PCDDS_pulses:
+                channel.write_instr(pulse)
+
+            for instruction in PCDDS_instructions:
+                channel.write_instruction(**instruction)
+
+    def setup_coupled(self,):
         # First pulses are 0V DC pulses
         # t_start and duration must be set but are irrelevant
         DC_0V_pulse = self.get_pulse_implementation(DCPulse('initial_0V',
@@ -121,8 +220,8 @@ class PCDDSInterface(InstrumentInterface):
             # Only trigger on first pulse
 
             # Use clock cycles for maximum accuracy
-            clk = self.instrument.clk
-            timing_offset = self.instrument.pulse_timing_offset
+            clk = self.instrument.ch1.clk
+            timing_offset = self.instrument.ch1.pulse_timing_offset
 
             t_min = min(self.pulse_sequence.t_list)
             cycles_min = int(round(t_min * clk))
@@ -140,6 +239,7 @@ class PCDDSInterface(InstrumentInterface):
                         pulse_implementation['pulse_idx'] = pulse_idx
                         pulse_implementation['next_pulse'] = pulse_idx + 1
                         pulse_implementation['duration'] = delta_cycles / clk
+                        channel.write_instr(pulse_implementation)
 
                         # Increment counters
                         pulse_idx += 1
@@ -176,7 +276,7 @@ class PCDDSInterface(InstrumentInterface):
                     cycles += delta_cycles
 
                 # Add a final DC pulse that requires triggering to restart
-                DC_0V_pulse.implement()
+                pulse_implementation = DC_0V_pulse.implementation.implement()
                 pulse_implementation['pulse_idx'] = pulse_idx
                 pulse_implementation['next_pulse'] = 1
                 channel.write_instr(pulse_implementation)
@@ -205,6 +305,21 @@ class PCDDSInterface(InstrumentInterface):
                         # Loop back to second pulse (ignore first 0V pulse)
                         pulse_implementation['next_pulse'] = 1
                     channel.write_instr(pulse_implementation)
+
+    def setup(self, **kwargs):
+        for channel in self.instrument.channels:
+            channel.instruction_sequence().clear()
+        self.instrument.channels.output_enable(False)
+        self.instrument.channels.pcdds_enable(True)
+
+        assert self.use_trig_in(), "Interface not yet programmed for pxi triggering"
+
+        # Perform different setup routine if FPGA image has pulses and
+        # instructions separated
+        if self.decoupled_mode:
+            self.setup_decoupled()
+        else:
+            self.setup_coupled()
 
         # targeted_pulse_sequence is the pulse sequence that is currently setup
         self.targeted_pulse_sequence = self.pulse_sequence
